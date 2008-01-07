1ae1d58043e6eecef5c75aa90f0e898e3af0754b
just_4_fun <just_4_fun@b60551fe-ae3b-0410-a507-ff091a16a36c>
2008-01-07 21:17:59 +0000

    struct carmen_laser_laser_message and carmen_laser_alive_message removed (put to comments).
    git-svn-id: https://svn.origo.ethz.ch/mrs@222 b60551fe-ae3b-0410-a507-ff091a16a36c

--- mrs/trunk/src/laser_messages.h
+++ mrs/trunk/src/laser_messages.h
@@ -50,14 +50,14 @@ extern "C" {
 		SIMULATED_LASER = 3, 
 		UMKNOWN_PROXIMITY_SENSOR = 99}
   carmen_laser_laser_type_t;
-  
+
   /** Possible remission values **/
   typedef enum {
     REMISSION_NONE       = 0, 
     REMISSION_DIRECT     = 1, 
     REMISSION_NORMALIZED = 2
   } carmen_laser_remission_type_t;
-  
+
   /** The configuration of the laser  **/
   typedef struct {
     carmen_laser_laser_type_t laser_type;   /**< what kind of laser is this **/
@@ -67,22 +67,22 @@ extern "C" {
     double maximum_range;                   /**< the maximum valid range of a measurement  **/
     double accuracy;                        /**< error in the range measurements **/  
     carmen_laser_remission_type_t remission_mode;  /* if and what kind of remission values are used */
-    
+
   } carmen_laser_laser_config_t;
 
-  
-  /** The laser message of the laser module (rawlaser) **/
+/* Markus:
+  / ** The laser message of the laser module (rawlaser) ** /
   typedef struct {
-    int id;                                /**< id of the laser (FLASER, RLASER, etc) **/
-    carmen_laser_laser_config_t config;    /**< configuration of the laser **/
-    int num_readings;                      /**< the number of beams of this laser message **/
-    float *range;                          /**< the measured distances (in meters) **/
-    int num_remissions;                    /**< the number of remission values. Should be 0 if config.remission_mode is OFF **/
-    float *remission;                      /**< the remission values **/
-    double timestamp;                      /**< timestamp when the message was read from the laser (typically the serial line) **/
-    char *host;                            /**< the host to which the laser is connected  **/
-  } carmen_laser_laser_message;
-
+    int id;                                / **< id of the laser (FLASER, RLASER, etc) ** /
+    carmen_laser_laser_config_t config;    / **< configuration of the laser ** /
+    int num_readings;                      / **< the number of beams of this laser message ** /
+    float *range;                          / **< the measured distances (in meters) ** /
+    int num_remissions;                    / **< the number of remission values. Should be 0 if config.remission_mode is OFF ** /
+    float *remission;                      / **< the remission values ** /
+    double timestamp;                      / **< timestamp when the message was read from the laser (typically the serial line) ** /
+    char *host;                            / **< the host to which the laser is connected  ** /
+  } carmen_laser_laser_message ;
+*/
 
 /** all raw laser messages have the same format **/
 #define      CARMEN_LASER_LASER_FMT       "{int,{int,double,double,double,double,double,int},int,<float:3>,int,<float:5>,double,string}"
@@ -110,16 +110,16 @@ extern "C" {
 #define      CARMEN_LASER_LASER5_NAME     "carmen_laser_laser5"
 #define      CARMEN_LASER_LASER5_FMT      CARMEN_LASER_LASER_FMT
 
-
-/** configuration of the laser **/
+/* Markus:
+/ ** configuration of the laser ** /
 typedef struct {
-  int frontlaser_stalled;  /**< laser1 (frontlaser) is stalled(=1) or is ok(=0) **/
-  int rearlaser_stalled;   /**< laser1 (rearlaser) is stalled(=1) or is ok(=0) **/
-  int laser3_stalled;      /**< laser3 is stalled(=1) or is ok(=0) **/
-  int laser4_stalled;      /**< laser4 is stalled(=1) or is ok(=0) **/
-  int laser5_stalled;      /**< laser5 is stalled(=1) or is ok(=0) **/
+  int frontlaser_stalled;  / **< laser1 (frontlaser) is stalled(=1) or is ok(=0) ** /
+  int rearlaser_stalled;   / **< laser1 (rearlaser) is stalled(=1) or is ok(=0) ** /
+  int laser3_stalled;      / **< laser3 is stalled(=1) or is ok(=0) ** /
+  int laser4_stalled;      / **< laser4 is stalled(=1) or is ok(=0) ** /
+  int laser5_stalled;      / **< laser5 is stalled(=1) or is ok(=0) ** /
 } carmen_laser_alive_message;
-  
+*/
 #define      CARMEN_LASER_ALIVE_NAME            "carmen_laser_alive"
 #define      CARMEN_LASER_ALIVE_FMT             "{int,int,int,int,int}"
 
