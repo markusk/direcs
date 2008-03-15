0deba052b51e31656e232931d20490f9efbcee51
just_4_fun <just_4_fun@b60551fe-ae3b-0410-a507-ff091a16a36c>
2008-03-15 15:55:47 +0000

    - support for saervos 3 to 6 added - init method added - init fof array added to constructor - signal message for GUI added
    git-svn-id: https://svn.origo.ethz.ch/mrs@442 b60551fe-ae3b-0410-a507-ff091a16a36c

--- mrs/trunk/src/servo.cpp
+++ mrs/trunk/src/servo.cpp
@@ -7,8 +7,13 @@ Servo::Servo(InterfaceAvr *i)
 	//------------------------------------------------------------------
 	interface1 = i;
 
-	// set initial servo position !!
-	
+	// init arrays
+	for (int servo=0; servo<NUMBEROFSERVOS; servo++)
+	{
+		servoStartPosition[servo] = 11;
+		servoDefaultPosition[servo] = 11;
+		servoEndPosition[servo] = 11;
+	}
 }
 
 
@@ -19,6 +24,14 @@ Servo::~Servo()
 
 void Servo::moveServo(unsigned char servo, unsigned char position)
 {
+	// *don't* move servo to a position out of the allowed range!!
+	if ( (position < servoStartPosition[servo]) || (position > servoEndPosition[servo]) )
+	{
+		emit message(QString("<b><font color=\"#FF0000\">Servo %1 position %2 out of allowed range (%3-%4)! (moveServo)</font>").arg(servo).arg(position).arg(servoStartPosition[servo]).arg(servoEndPosition[servo]));
+		return;
+	}
+
+
 	switch (servo)
 	{
 		case SERVO1:
@@ -49,8 +62,64 @@ void Servo::moveServo(unsigned char servo, unsigned char position)
 				return;
 			}
 			break;
+		case SERVO3:
+			// which servo
+			if (interface1->sendChar(SET_SERVO3) == false)
+			{
+				qDebug("ERROR sending to serial port (Servo)");
+				return;
+			}
+			// the position
+			if (interface1->sendChar(position) == false)
+			{
+				qDebug("ERROR sending to serial port (Servo)");
+				return;
+			}
+			break;
+		case SERVO4:
+			// which servo
+			if (interface1->sendChar(SET_SERVO4) == false)
+			{
+				qDebug("ERROR sending to serial port (Servo)");
+				return;
+			}
+			// the position
+			if (interface1->sendChar(position) == false)
+			{
+				qDebug("ERROR sending to serial port (Servo)");
+				return;
+			}
+			break;
+		case SERVO5:
+			// which servo
+			if (interface1->sendChar(SET_SERVO5) == false)
+			{
+				qDebug("ERROR sending to serial port (Servo)");
+				return;
+			}
+			// the position
+			if (interface1->sendChar(position) == false)
+			{
+				qDebug("ERROR sending to serial port (Servo)");
+				return;
+			}
+			break;
+		case SERVO6:
+			// which servo
+			if (interface1->sendChar(SET_SERVO6) == false)
+			{
+				qDebug("ERROR sending to serial port (Servo)");
+				return;
+			}
+			// the position
+			if (interface1->sendChar(position) == false)
+			{
+				qDebug("ERROR sending to serial port (Servo)");
+				return;
+			}
+			break;
 		default:
-			qDebug("ERROR: servo number not supportet (setServoPosition(%d) )", servo);
+			emit message(QString("<b><font color=\"#FF0000\">ERROR: Servo number %1 not supportet (moveServo)</font>").arg(servo));
 			break;
 	}
 }
@@ -72,5 +141,14 @@ void Servo::setServoPosition(int servo, unsigned char type, unsigned char positi
 			servoDefaultPosition[servo] = position;
 			return;
 			break;
+	}
+}
+
+
+void Servo::init(void)
+{
+	for (int servo=0; servo<NUMBEROFSERVOS; servo++)
+	{
+		moveServo(servo+1, servoDefaultPosition[servo]);
 	}
 }
--- mrs/trunk/src/servo.h
+++ mrs/trunk/src/servo.h
@@ -18,12 +18,17 @@ class Servo : public QObject
 		~Servo();
 		
 		/**
-		Sets the start position of a servo.
+		Sets the servos maximum start, end and its default position.
 		@param servo is the servo number.
 		@param type can be SVSTART, SVEND or SVDEFAULT
 		@param position is the position (0 - 255).
 		 */
 		void setServoPosition(int servo, unsigned char type, unsigned char position);
+		
+		/**
+		Moves all servos into their default positions.
+		 */
+		void init(void);
 
 
 	public slots:
@@ -33,6 +38,13 @@ class Servo : public QObject
 		@param position is the position (0 - 255).
 		 */
 		void moveServo(unsigned char servo, unsigned char position);
+
+
+	signals:
+		/**
+		Emits a message to be displayed in the GUI or be used somewhere else (e.g. error message).
+		*/
+		void message(QString text);
 
 
 	private:
@@ -49,12 +61,21 @@ class Servo : public QObject
 		static const unsigned char SVEND     = 1;
 		static const unsigned char SVDEFAULT = 2;
 		
-		static const unsigned char SERVO1 = 10; // Servo 1
-		static const unsigned char SERVO2 = 20; // Servo 2
+		//! the servo numbers
+		static const unsigned char SERVO1 = 1;
+		static const unsigned char SERVO2 = 2;
+		static const unsigned char SERVO3 = 3;
+		static const unsigned char SERVO4 = 4;
+		static const unsigned char SERVO5 = 5;
+		static const unsigned char SERVO6 = 6;
 
 		//! the command for the microcontroller
-		static const unsigned char SET_SERVO1 = 45;
-		static const unsigned char SET_SERVO2 = 46;
+		static const unsigned char SET_SERVO1 = 42;
+		static const unsigned char SET_SERVO2 = 43;
+		static const unsigned char SET_SERVO3 = 44;
+		static const unsigned char SET_SERVO4 = 45;
+		static const unsigned char SET_SERVO5 = 46;
+		static const unsigned char SET_SERVO6 = 47;
 };
 
 #endif
