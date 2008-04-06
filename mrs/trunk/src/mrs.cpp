5ca51a4ce4a03772c56055c87432337a662ff534
just_4_fun <just_4_fun@b60551fe-ae3b-0410-a507-ff091a16a36c>
2008-04-06 18:51:42 +0000

    test signal also connected to camThread slot
    git-svn-id: https://svn.origo.ethz.ch/mrs@588 b60551fe-ae3b-0410-a507-ff091a16a36c

--- mrs/trunk/src/mrs.cpp
+++ mrs/trunk/src/mrs.cpp
@@ -116,9 +116,10 @@ void Mrs::init()
 	connect(gui, SIGNAL(shutdown()), this, SLOT(shutdown()));
 	
 	//--------------------------------------------------------------------------
-	// call the test method at the test button
+	// call (a) test method(s) when clicking the test button
 	//--------------------------------------------------------------------------
 	connect(gui, SIGNAL(test()), this, SLOT(test()));
+	connect(gui, SIGNAL(test()), camThread, SLOT(test()));
 	
 	//--------------------------------------------------------------------------
 	// set the motor speed, when signal comes from Gui
@@ -2970,7 +2971,7 @@ void Mrs::test()
 	#ifdef _TTY_POSIX_
 	// Say some text;
 	QDateTime now = QDateTime::currentDateTime();
-	emit speak(tr("Hello Markus. Today it's the %1 of %2, %3. The time is %4:%5.").arg(now.toString("d")).arg(now.toString("MMMM")).arg(now.toString("yyyy")).arg(now.toString("h")).arg(now.toString("m")));
+//	emit speak(tr("Hello Markus. Today it's the %1 of %2, %3. The time is %4:%5.").arg(now.toString("d")).arg(now.toString("MMMM")).arg(now.toString("yyyy")).arg(now.toString("h")).arg(now.toString("m")));
 	#endif
 
 }
