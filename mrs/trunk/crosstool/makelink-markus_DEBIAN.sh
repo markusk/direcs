f86f5d66a49da2b333ee65a6be9538ef9b47f7f0
markusk <github@direcs.de>
2008-12-30 14:36:28 +0100

    Next successfull run, with no lib or runtime error! :-D
    Crosstool scripts added.
    TODO: Call a "main loop".

---
+++ mrs/trunk/crosstool/makelink-markus_DEBIAN.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#
+# to use in
+# NEW:  /home/markus/develop/nslu2/crosstool/gcc-4.1.0-glibc-2.3.2/arm-linux/bin
+
+ln -s arm-linux-addr2line arm-linux-addr2line
+ln -s arm-linux-ar arm-linux-ar 
+ln -s arm-linux-as arm-linux-as 
+ln -s arm-linux-c++ arm-linux-c++ 
+ln -s arm-linux-c++filt arm-linux-c++filt 
+ln -s arm-linux-cpp arm-linux-cpp 
+ln -s arm-linux-g++ arm-linux-g++ 
+ln -s arm-linux-gcc arm-linux-gcc 
+ln -s arm-linux-gcc-4.1.0 arm-linux-gcc-4.1.0
+ln -s arm-linux-gccbug arm-linux-gccbug 
+ln -s arm-linux-gcov arm-linux-gcov 
+ln -s arm-linux-gprof arm-linux-gprof
+ln -s arm-linux-gdb arm-linux-gdb 
+ln -s arm-linux-gdbtui arm-linux-gdbtui 
+ln -s arm-linux-ld arm-linux-ld 
+ln -s arm-linux-nm arm-linux-nm 
+ln -s arm-linux-objcopy arm-linux-objcopy 
+ln -s arm-linux-objdump arm-linux-objdump 
+ln -s arm-linux-ranlib arm-linux-ranlib 
+ln -s arm-linux-readelf arm-linux-readelf 
+ln -s arm-linux-run arm-linux-run 
+ln -s arm-linux-size arm-linux-size 
+ln -s arm-linux-strings arm-linux-strings 
+ln -s arm-linux-strip arm-linux-strip 
