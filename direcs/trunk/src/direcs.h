f388a649775dbb03094ba75eddbadbbe38ac80b0
markusk <github@direcs.de>
2009-04-12 01:08:44 +0200

    Whitespaces removed

--- direcs/trunk/src/direcs.h
+++ direcs/trunk/src/direcs.h
@@ -73,7 +73,7 @@
 
 /**
 \brief The main class of the whole robot programm.
- 
+
  All objects are created here. This method also starts the GUI and its splash screen.
 */
 class Direcs : public QObject
@@ -90,33 +90,33 @@ class Direcs : public QObject
 		~Direcs();
 		static const Qt::Alignment splashPosition;
 		static const QColor splashColor;
-		
+
 		/**
 		*/
 		void init();
-	
+
 		/**
 		Returns the status of the main program. True, if the simulationMode ist active.
 		*/
 		bool simulationMode() const;
-		
+
 		/**
 		Currently not in use...
 		*/
 		bool noHardwareErrorMessages;
-		
+
 		/**
 		Configures if an exit dialog should be shown, when exiting the main program.
 		*/
 		bool exitDialog;
 
-		
+
 		// Unix signal handlers.
 		static void hupSignalHandler(int unused);
 		static void termSignalHandler(int unused);
-		
+
 	/*
-	protected:	
+	protected:
 		bool event(QEvent *event);
 	*/
 
@@ -126,7 +126,7 @@ class Direcs : public QObject
 		@param command
 		 */
 		void drive(const unsigned char command);
-		
+
 		/**
 		The locialUnit handles the behavior of the robot. This slot is called if an obstacle is detected from the obstacleCheckThread.
 		It lets the robot "react" depending on which sensor made the alarm. The number of alarms are also counted.
@@ -134,7 +134,7 @@ class Direcs : public QObject
 		@param timestamp is the timestmap when the signal was emitted.
 		*/
 		void logicalUnit(int sensorAlarm, QDateTime timestamp);
-		
+
 		/**
 		This method moves the camera and/or the robots eyes in the direction of a detected face.
 		@sa camThread::faceDetected()
@@ -146,80 +146,80 @@ class Direcs : public QObject
 		@param state has to be Qt::Checked to enable the detection. All other states disable.
 		*/
 		void enableFaceTracking(int state);
-		
+
 		/**
 		Shows the sensor data in a graphical way (progressBars) and as a number in the GUI.
 		This slot is called from the Signal sensorDataComplete from the sensorThread.
 		*/
 		void showSensorData();
-		
+
 		/**
 		Shows the yes-no exit dialog for the main program.
 		This slot is called when massive communication errors occur while talking to the robots interface!
 		*/
 		void showExitDialog();
-		
+
 		/**
 		Enables or disables the listening for the robot remote control.
 		This slot is called from an event from the GUI (enable remote button).
 		@param status
 		*/
 		void enableRemoteControlListening(bool status);
-		
+
 		/**
 		This slot executes remote commands it receives via network (e.g. WLAN).
 		*/
 		void executeRemoteCommand(QString command);
-		
+
 		/**
 		This slot executes commands from a connected joystick.
 		*/
 		void executeJoystickCommand(int axisNumber, int axisValue);
 		void executeJoystickCommand(int buttonNumber, bool buttonState);
-		
+
 		/**
 		This slot enables or disables the simulation mode.
 		@param status
 		*/
 		void setSimulationMode(bool status);
-		
+
 		/**
 		Shows a text on the current splash screen.
 		*/
 		void showSplashMessage(QString text);
-		
+
 		/**
 		Finishes the splash screen and displays the main window.
 		*/
 		void finishSplash();
-		
+
 		/**
 		This slot sets (stores) the robots (circuits) state within this class.
 		@param state can be ON or OFF
 		 */
 		void setRobotState(bool state);
-		
+
 		/**
 		Speaks a text with festival. All HTML-Code in the parameter (text) is also removed internally).
 		@param text is the text to speak.
 		*/
 		// FIXME: use speakThread instead!!
 		void speak(QString text);
-		
+
 		/**
 		Ends the programm, after asking "really?".
 		Calls the destructor of this class, after saving settings and some other exit stuff.
 		*/
 		void shutdown();
-		
+
 		/**
 		The method for the test button in the GUI.
 		 */
 		void test();
-		
+
 		void handleSigHup();
 		void handleSigTerm();
-		
+
 
 
 	signals:
@@ -228,28 +228,28 @@ class Direcs : public QObject
 		@sa logicalUnit()
 		*/
 		void showPreferredDirection(QString direction);
-		
+
 		/**
 		Shows the face tracking direction in a lable.
 		 */
 		void showFaceTrackDirection(QString direction);
-	
+
 		/**
 		This is for the robots head.
 		@param direction where to look can be LEFT, RIGHT...
 		*/
 		void look(QString direction);
-		
+
 		/**
 		Emits a speak signal. This signal is sent to the speakThread.
 		*/
 		//void speak(QString text);
-		
+
 		/**
 		Initialize the robots basic circuit.
 		*/
 		void initCircuit();
-		
+
 		/**
 		Sends a string over the network.
 		*/
@@ -261,13 +261,13 @@ class Direcs : public QObject
 		Reads all settings for the robot from an ini-file.
 		*/
 		void readSettings();
-		
+
 		static int sighupFd[2];
 		static int sigtermFd[2];
 
 		QSocketNotifier *snHup;
 		QSocketNotifier *snTerm;
-		
+
 		mutable QMutex *mutex; // make the threads thread-safe (e.g. senorThread, servo...)
 
 		Gui *gui;
@@ -307,7 +307,7 @@ class Direcs : public QObject
 		bool laserScannerFrontFound;
 		bool laserScannerRearFound;
 		bool testDriveMode; /// is enabled, when the robot is controlled via joystick
-		bool mecanumDriveMode; /// is enabled, when the robot is controlled via joystick and should use special mecanum wheel driving 
+		bool mecanumDriveMode; /// is enabled, when the robot is controlled via joystick and should use special mecanum wheel driving
 		bool servoTestMode;
 		bool eyeTestMode;
 		int currentTestServo;
@@ -327,10 +327,10 @@ class Direcs : public QObject
 
 		/// The splash screen time of view in ms
 		static const unsigned int SPLASHTIME  = 2000;
-		
+
 		static const bool ON  = true;   /** For motor "ON" */
 		static const bool OFF = false;  /** For motor "OFF" */
-		
+
 		/// Some driving directions for the robot. @sa drive() [Slot]
 		static const unsigned char FORWARD		= 1;
 		static const unsigned char BACKWARD		= 2;
@@ -357,7 +357,11 @@ class Direcs : public QObject
 		static const unsigned char MOTOR4FW		= 19;
 		static const unsigned char MOTOR4BW		= 20;
 		static const unsigned char MOTOR4OFF	= 21;
-		
+
+		static const unsigned char READ_AXIS_X = 61;
+		static const unsigned char READ_AXIS_Y = 62;
+		static const unsigned char READ_AXIS_Z = 63;
+
 		/**
 		Give the sensors some names
 		DONT CHANGE THIS NUMBERS!
@@ -371,19 +375,19 @@ class Direcs : public QObject
 		static const short int SENSOR6 = 32;
 		static const short int SENSOR7 = 64;
 		static const short int SENSOR8 = 128;
-		
+
 		////The ultrasonic sensor
 		static const short int SENSOR16 = 256;
-		
+
 		/// Value, if no sensor has a value to react
 		static const short int NONE = 0;
-		
+
 		static const short int OBSTACLEFRONTLEFT = 512;
 		static const short int OBSTACLEFRONTRIGHT = 1024;
 		static const short int OBSTACLESEVERYWHEREINFRONT = 2048;
-		
+
 		static const short int MAXFALSEALARMS = 2; /// number of false alarms which are allowed in the logical unit
-		
+
 		static const unsigned char CLOCKWISE        = 0;  /// Motor direction "CLOCKWISE"
 		static const unsigned char COUNTERCLOCKWISE = 1;  /// Motor direction "COUNTERCLOCKWISE"
 		static const unsigned char SAME             = 3;  /// Motor direction/power "same like before"
@@ -391,11 +395,11 @@ class Direcs : public QObject
 		static const unsigned char MOTOR2           = 20; /// Motor 2 front right
 		static const unsigned char MOTOR3           = 30; /// Motor 3 back left
 		static const unsigned char MOTOR4           = 40; /// Motor 4 back right
-		
+
 		/// The motor sensors
 		static const short int MOTORSENSOR1 = 0;
 		static const short int MOTORSENSOR2 = 1;
-		
+
 		/// The servos
 		static const unsigned char NUMBEROFSERVOS = 6; // TODO: also defined in servo.h !!!
 		/// the servo numbers
@@ -405,7 +409,7 @@ class Direcs : public QObject
 		static const unsigned char SERVO4 = 3;
 		static const unsigned char SERVO5 = 4;
 		static const unsigned char SERVO6 = 5;
-		
+
 		/// the possible servo data
 		static const unsigned char SVSTART   = 0;
 		static const unsigned char SVEND     = 1;
@@ -413,7 +417,7 @@ class Direcs : public QObject
 		static const unsigned char SVCURRENT = 3;
 		static const unsigned char SVMIN     = 4;
 		static const unsigned char SVMAX     = 5;
-		
+
 		// TODO: put axis numbers to ini-file
 		/// Joystick axis numbers
 		static const int JOYSTICKAXISY = 2;
@@ -422,7 +426,7 @@ class Direcs : public QObject
 		static const int JOYSTICKAXIS2Y = 5;
 		/// Joystick conversion divisor. Converts the joystick axis value to the robot speed.
 		static const int JOYSTICKDIVISOR = 128;
-		
+
 		/**
 		For the laserThread
 		*/
