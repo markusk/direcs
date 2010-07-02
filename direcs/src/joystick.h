e6ca5abc4e54f835c8dc4c1b385759965a34c06b
markusk <github@direcs.de>
2010-07-03 00:23:01 +0200

    Removed unused code

--- direcs/src/joystick.h
+++ direcs/src/joystick.h
@@ -38,11 +38,6 @@
 
 #ifdef Q_OS_MAC // joystick support for Mac OS:
 #include "ysjoyreader.h"
-
-#include <ctype.h>
-#include <sys/errno.h>
-#include <sysexits.h>
-#include <IOKit/hid/IOHIDLib.h>
 #endif
 
 #include <unistd.h>
@@ -135,94 +130,4 @@ class Joystick : public QThread
 #endif
 };
 
-
-/*
-#ifdef Q_OS_MAC // joystick support for Mac OS:
-static const int YsJoyReaderMaxNumAxis = 6;
-static const int YsJoyReaderMaxNumButton = 32;
-static const int YsJoyReaderMaxNumHatSwitch = 4;
-#endif
-
-class YsJoyReaderElement
-{
-public:
-	int exist;
-	IOHIDElementRef elem;
-	int value;
-
-	YsJoyReaderElement();
-};
-
-class YsJoyReaderAxis : public YsJoyReaderElement
-{
-public:
-	int min,max;
-	int scaledMin,scaledMax;
-	int calibCenter,calibMin,calibMax;
-
-	YsJoyReaderAxis();
-	double GetCalibratedValue(void) const;
-
-	void CaptureCenter(void);
-	void BeginCaptureMinMax(void);
-	void CaptureMinMax(void);
-	void CenterFromMinMax(void);
-};
-
-class YsJoyReaderButton : public YsJoyReaderElement
-{
-public:
-	YsJoyReaderButton();
-};
-
-class YsJoyReaderHatSwitch : public YsJoyReaderElement
-{
-public:
-	YsJoyReaderHatSwitch();
-	int valueNeutral;
-	int value0Deg;
-	int value90Deg;
-	int value180Deg;
-	int value270Deg;
-	int GetDiscreteValue(void) const;
-};
-
-class  YsJoyReader
-{
-public:
-	static IOHIDManagerRef hidManager;
-	static CFMutableArrayRef devArray;
-
-	int joyId;
-	IOHIDDeviceRef hidDev;
-	char regPath[512];
-
-	YsJoyReaderAxis axis[YsJoyReaderMaxNumAxis];
-	YsJoyReaderButton button[YsJoyReaderMaxNumButton];
-	YsJoyReaderHatSwitch hatSwitch[YsJoyReaderMaxNumHatSwitch];
-
-	YsJoyReader();
-	int SetUpInterface(int joyId,IOHIDDeviceRef hidDev);
-	void Read(void);
-	void ReleaseInterface(void);
-
-	int WriteCalibInfoFile(FILE *fp) const;
-	int ReadCalibInfoFile(FILE *fp);
-
-protected:
-	void AddAxis(int axisId,IOHIDElementRef elem,int min,int max,int scaledMin,int scaledMax);
-
-public:
-	static int SetUpJoystick(int &nJoystick,YsJoyReader joystick[],int maxNumJoystick);
-};
-
-
-int YsJoyReaderSetUpJoystick(int &nJoystick,YsJoyReader joystick[],int maxNumJoystick);
-
-FILE *YsJoyReaderOpenJoystickCalibrationFile(const char mode[]);
-
-int YsJoyReaderSaveJoystickCalibrationInfo(int nJoystick,YsJoyReader joystick[]);
-int YsJoyReaderLoadJoystickCalibrationInfo(int nJoystick,YsJoyReader joystick[]);
-#endif // mac os
-*/
 #endif
