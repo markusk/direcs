32d1c4a9a21278eb82175e42ffacfa4e9ca690fe
markusk <github@direcs.de>
2010-07-18 23:06:50 +0200

    Include path for QtOpenGl updated (changed since Qt 4.6.3)

--- direcs/src/QtGLContext.h
+++ direcs/src/QtGLContext.h
@@ -21,7 +21,7 @@
 #ifndef GLCONTEXT_H_
 #define GLCONTEXT_H_
 
-#include <Qt/qgl.h>
+#include <QtOpenGL>
 #include <QString>
 #include <QtGui/QMouseEvent>
 
@@ -34,7 +34,7 @@ class QtGLContext : public QGLWidget
 {
 	Q_OBJECT
 
-	
+
 public:
 	//! Constructor
 	QtGLContext(QWidget* parent = 0, const QGLWidget* sharedWidget = 0, Qt::WFlags f = 0 );
@@ -44,7 +44,7 @@ public:
 	//! set image parameters 'flipped' is a vertical flip!
 	//bool setImage(unsigned char* imgP, const int width, const int height, const int pixeldepth, const bool flipped = false);
 	bool setImage(unsigned char* imgP, const bool flipped = false);
-	
+
 	//! set the image data (for not doing this at every setImage call!)
 	void setImageData(const int width, const int height, const int pixeldepth);
 
@@ -56,15 +56,15 @@ public:
 	/**
 	Enables or disables the mirror mode for the camera. When activated, the camera live image is mirrored horicontally.
 	@param state has to be Qt::Checked to enable the detection. All other states disable.
-	*/ 
+	*/
 	void enableMirrorMode(int state);
 
-	
+
 signals:
 	void boxCreated(int x, int y, int x2, int y2);
 	void saveImage();
 
-	
+
 protected:
 	// standard qt methods
 	virtual void paintGL();
@@ -94,9 +94,9 @@ private:
 	//! image parameters
 	unsigned int m_width, m_height, m_pixeldepth;
 	//! if set to true image will be flipped vertically
-    bool         m_flipped;
+	bool         m_flipped;
 	//! if set to true image will be flipped horizontally
-    bool         m_mirrored;
+	bool         m_mirrored;
 	//! width of the texture buffer
 	unsigned int m_texWidth;
 	//! height of the texture buffer
@@ -106,7 +106,7 @@ private:
 	//! clipping rectangle for texture
 	float m_texUpperLeftX, m_texUpperLeftY, m_texLowerRightX, m_texLowerRightY;
 	// box parameters (for mouse dragging)
-	QPoint m_entry, m_exit;	
+	QPoint m_entry, m_exit;
 	bool m_mousePressed;
 	// "the image" (texture)
 	unsigned char* textureGL;
