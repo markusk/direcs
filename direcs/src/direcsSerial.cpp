074a3bb2a0ea425c14a17f92eb04cdf8cfb81452
markusk <github@direcs.de>
2010-07-24 23:55:22 +0200

    Debugging direcsSerial - tv_sec set to 2 - amountRead qdebug added

--- direcs/src/direcsSerial.cpp
+++ direcs/src/direcsSerial.cpp
@@ -524,7 +524,7 @@ int DirecsSerial::readAtmelPort(unsigned char *buf, int nChars)
 	while (nChars > 0)
 	{
 		// Timeout is not changed by select(), and may be reused on subsequent calls, however it is good style to re-initialize it before each invocation of select().
-		t.tv_sec = 0;
+		t.tv_sec = 2;
 		t.tv_usec = READ_TIMEOUT_ATMEL;
 		FD_ZERO(&set);
 		FD_SET(mDev_fd, &set);
@@ -542,6 +542,7 @@ int DirecsSerial::readAtmelPort(unsigned char *buf, int nChars)
 		// read from the serial device
 		amountRead = read(mDev_fd, buf, nChars);
 
+if (amountRead<=0)
 qDebug("amountRead=%d", amountRead);
 
 		if(amountRead < 0 && errno != EWOULDBLOCK)
