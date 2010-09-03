62b40bbc66c3716f2dc8d18fc61519ce45d11dc5
markusk <github@direcs.de>
2010-09-04 00:33:15 +0200

    Added Atmel code for reading driven distance for motor 1 and 2.

--- direcs-avr/main.c
+++ direcs-avr/main.c
@@ -349,6 +349,20 @@ int main(void)
 				sendUInt( readADC(SENSORMOTOR2) );
 			}
 			
+			// READ_MOTOR_DISTANCE1
+			if (strcmp(stringbuffer, "*md1#") == 0)
+			{
+				// send driven distance of motor 1 encoder over serial port
+				sendUInt( rightDistanceCounter );
+			}
+			
+			// READ_MOTOR_DISTANCE2
+			if (strcmp(stringbuffer, "*md2#") == 0)
+			{
+				// send driven distance of motor 2 encoder over serial port
+				sendUInt( leftDistanceCounter );
+			}
+			
 		} // RXcompleted
     } // while (1)
 
