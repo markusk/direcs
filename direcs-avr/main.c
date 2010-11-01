d3404cd22159c1e7595cefde90b741c9136e1d1f
markusk <github@direcs.de>
2010-11-01 16:48:39 +0100

    Added command BOTBACKWARD.

--- direcs-avr/main.c
+++ direcs-avr/main.c
@@ -781,6 +781,34 @@ int main(void)
 				// answer with "ok"
 				put_string("*bdf#");
 			}
+			else
+			// BOTBACKWARD = "bot drive backward"
+			if (strcmp(stringbuffer, "*bdb#") == 0)
+			{
+				// MOTOR 1 COUNTERCLOCKWISE
+				// set Motor1 A bit
+				PORTL |= (1<<PIN0);
+				// delete Motor1 B bit
+				PORTL &= ~(1<<PIN1);
+				// MOTOR 2 CLOCKWISE
+				// delete Motor2 A bit
+				PORTL &= ~(1<<PIN2);
+				// set Motor2 B bit
+				PORTL |= (1<<PIN3);
+				// MOTOR 3 COUNTERCLOCKWISE
+				// set Motor3 A bit
+				PORTL |= (1<<PIN6);
+				// delete Motor3 B bit
+				PORTL &= ~(1<<PIN7);
+				// MOTOR 4 CLOCKWISE
+				// delete Motor4 A bit
+				PORTD &= ~(1<<PIN6);
+				// set Motor4 B bit
+				PORTD |= (1<<PIN7);
+
+				// answer with "ok"
+				put_string("*bdb#");
+			}
 		
 		} // RXcompleted
     } // while (1)
@@ -817,30 +845,8 @@ int main(void)
 			case BOTWAIT:
 			case BOTSTART:
 			case BOTFORWARD:
-
-				
 			case BOTBACKWARD:
-				// MOTOR 1 COUNTERCLOCKWISE
-				// set Motor1 A bit
-				PORTL |= (1<<PIN0);
-				// delete Motor1 B bit
-				PORTL &= ~(1<<PIN1);
-				// MOTOR 2 CLOCKWISE
-				// delete Motor2 A bit
-				PORTL &= ~(1<<PIN2);
-				// set Motor2 B bit
-				PORTL |= (1<<PIN3);
-				// MOTOR 3 COUNTERCLOCKWISE
-				// set Motor3 A bit
-				PORTL |= (1<<PIN6);
-				// delete Motor3 B bit
-				PORTL &= ~(1<<PIN7);
-				// MOTOR 4 CLOCKWISE
-				// delete Motor4 A bit
-				PORTD &= ~(1<<PIN6);
-				// set Motor4 B bit
-				PORTD |= (1<<PIN7);
-				break;
+
 				
 			case BOTLEFT:
 				// MOTOR 1 COUNTERCLOCKWISE
