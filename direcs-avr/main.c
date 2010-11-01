99ee66e17632c148fb311ee1eba02881bf15292b
markusk <github@direcs.de>
2010-11-01 17:10:55 +0100

    Added new code for commands TURNLEFT and TURNRIGHT. Moved "end statement" 'return 0' to the new end position of the complete new serial code.

--- direcs-avr/main.c
+++ direcs-avr/main.c
@@ -865,19 +865,74 @@ int main(void)
 				// answer with "ok"
 				put_string("*bdr#");
 			}
+			else
+			// TURNLEFT = "bot turn left"
+			if (strcmp(stringbuffer, "*btl#") == 0)
+			{
+				// MOTOR 1 COUNTERCLOCKWISE
+				// set Motor1 A bit
+				PORTL |= (1<<PIN0);
+				// delete Motor1 B bit
+				PORTL &= ~(1<<PIN1);
+				// MOTOR 2 CLOCKWISE
+				// delete Motor2 A bit
+				PORTL &= ~(1<<PIN2);
+				// set Motor2 B bit
+				PORTL |= (1<<PIN3);
+				// MOTOR 3 COUNTERCLOCKWISE
+				// set Motor3 A bit
+				PORTL |= (1<<PIN6);
+				// delete Motor3 B bit
+				PORTL &= ~(1<<PIN7);
+				// MOTOR 4 CLOCKWISE
+				// delete Motor4 A bit
+				PORTD &= ~(1<<PIN6);
+				// set Motor4 B bit
+				PORTD |= (1<<PIN7);
+
+				// answer with "ok"
+				put_string("*btl#");
+			}
+			else
+			// TURNRIGHT = "bot turn right"
+			if (strcmp(stringbuffer, "*btr#") == 0)
+			{
+				// MOTOR 1 CLOCKWISE
+				// delete Motor1 A bit
+				PORTL &= ~(1<<PIN0);
+				// set Motor1 B bit
+				PORTL |= (1<<PIN1);
+				// MOTOR 2 COUNTERCLOCKWISE
+				// set Motor2 A bit
+				PORTL |= (1<<PIN2);
+				// delete Motor2 B bit
+				PORTL &= ~(1<<PIN3);
+				// MOTOR 3 CLOCKWISE
+				// delete Motor3 A bit
+				PORTL &= ~(1<<PIN6);
+				// set Motor3 B bit
+				PORTL |= (1<<PIN7);
+				// MOTOR 4 COUNTERCLOCKWISE
+				// set Motor4 A bit
+				PORTD |= (1<<PIN6);
+				// delete Motor4 B bit
+				PORTD &= ~(1<<PIN7);
+
+				// answer with "ok"
+				put_string("*btr#");
+			}
 		
 		} // RXcompleted
     } // while (1)
 
 
-	//-----------------------------------------------------
-	//-----------------------------------------------------
+	// this line is never reached!
+	return 0;
+}
+
 
 
 /*
-	while(1)
-	{
-		
 		switch (value)
 		{
 			case MOTOR1_OFF:
@@ -906,16 +961,10 @@ int main(void)
 			case BOTRIGHT:
 			case BOTSTOP:
 			case BOTWAIT:
-
-				
 			case BOTTURNLEFT:
-				break;
-				
 			case BOTTURNRIGHT:
-				break;
+		
 				
-				
-				//-------------------------------
 			case SET_SERVO1:
 				// wait for the (second) value to set the pwm!
 				value = UsartReceive();
@@ -987,14 +1036,7 @@ int main(void)
 				UsartTransmit( (uint8_t) bit_is_set(PINK,PIN0) );
 				break;
 		}
-	} // while (1)
 */
-
-
-	// this line is never reached!
-	return 0;
-}
-
 
 
 void sendUInt(uint16_t value)
