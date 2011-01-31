34c973465121e0569cb21443477ef7d8359512ab
markusk <github@direcs.de>
2011-01-31 22:18:04 +0100

    camThread updated (stuff removed which is now in direcsKinect)

--- direcs/src/camThread.cpp
+++ direcs/src/camThread.cpp
@@ -20,7 +20,7 @@
 
 #include "camThread.h"
 
-/*
+
 CamThread::CamThread() : QThread()
 {
 	stopped = false;
@@ -35,17 +35,8 @@ CamThread::CamThread() : QThread()
 	Mat depthf  (Size(640,480),CV_8UC1);
 	Mat rgbMat(Size(640,480),CV_8UC3,Scalar(0));
 	Mat ownMat(Size(640,480),CV_8UC3,Scalar(0));
-
-	for (unsigned int i = 0 ; i < 2048 ; i++)
-	{
-		float v = i/2048.0;
-		v = std::pow(v, 3)* 6;
-		m_gamma[i] = v*6*256;
-	}
-
-
 }
-*/
+
 
 CamThread::~CamThread()
 {
@@ -59,70 +50,6 @@ CamThread::~CamThread()
 void CamThread::stop()
 {
 	stopped = true;
-}
-
-
-void CamThread::VideoCallback(void* _rgb, uint32_t timestamp)
-{
-	//
-	// Do not call directly even in child
-	//
-	std::cout << "RGB callback" << std::endl;
-	m_rgb_mutex.lock();
-	uint8_t* rgb = static_cast<uint8_t*>(_rgb);
-	rgbMat.data = rgb;
-	m_new_rgb_frame = true;
-	m_rgb_mutex.unlock();
-}
-
-
-void CamThread::DepthCallback(void* _depth, uint32_t timestamp)
-{
-	//
-	// Do not call directly even in child
-	//
-	std::cout << "Depth callback" << std::endl;
-	m_depth_mutex.lock();
-	uint16_t* depth = static_cast<uint16_t*>(_depth);
-	depthMat.data = (uchar*) depth;
-	m_new_depth_frame = true;
-	m_depth_mutex.unlock();
-}
-
-
-bool CamThread::getVideo(Mat& output)
-{
-	m_rgb_mutex.lock();
-	if(m_new_rgb_frame)
-	{
-		cv::cvtColor(rgbMat, output, CV_RGB2BGR);
-		m_new_rgb_frame = false;
-		m_rgb_mutex.unlock();
-		return true;
-	}
-	else
-	{
-		m_rgb_mutex.unlock();
-		return false;
-	}
-}
-
-
-bool CamThread::getDepth(Mat& output)
-{
-	m_depth_mutex.lock();
-	if(m_new_depth_frame)
-	{
-		depthMat.copyTo(output);
-		m_new_depth_frame = false;
-		m_depth_mutex.unlock();
-		return true;
-	}
-	else
-	{
-		m_depth_mutex.unlock();
-		return false;
-	}
 }
 
 
--- direcs/src/camThread.h
+++ direcs/src/camThread.h
@@ -40,70 +40,22 @@
 
 using namespace cv;
 using namespace std;
-
 //-------------------------------------------------------------------
 
 
-/*!
+/**
 \brief This class gets a live picture from a Kinect camera.
-This class uses the freenect library and the OpenCV library for grabbing pictures and also for face detection.
+This class uses the QThread techniquesand then some OpenCV stuff on the live picture; we will see...
 */
-class CamThread : public QThread, public Freenect::FreenectDevice
+class CamThread : public QThread
 {
 	Q_OBJECT
 
 
 	public:
-		CamThread(freenect_context *_ctx, int _index) : Freenect::FreenectDevice(_ctx, _index),
-			m_buffer_depth(FREENECT_DEPTH_11BIT_SIZE),
-			m_buffer_rgb(FREENECT_VIDEO_RGB_SIZE),
-			m_gamma(2048),
-			m_new_rgb_frame(false),
-			m_new_depth_frame(false),
-			depthMat(cv::Size(640,480),CV_16UC1), rgbMat(cv::Size(640,480), CV_8UC3,Scalar(0)), ownMat(cv::Size(640,480), CV_8UC3,cv::Scalar(0))
-		{
-			stopped = false;
-			initDone = false;;
-			cameraIsOn = false;
-			faceDetectionIsEnabled = false;
-			faceDetectionWasActive = false;
-			haarClassifierCascadeFilename = "none";
-
-			Mat depthMat(cv::Size(640,480), CV_16UC1);
-			Mat depthf(cv::Size(640,480), CV_8UC1);
-			Mat rgbMat(cv::Size(640,480), CV_8UC3, cv::Scalar(0));
-			Mat ownMat(cv::Size(640,480), CV_8UC3, cv::Scalar(0));
-
-			for (unsigned int i = 0 ; i < 2048 ; i++)
-			{
-				float v = i/2048.0;
-				v = std::pow(v, 3)* 6;
-				m_gamma[i] = v*6*256;
-			}
-
-
-		//	Freenect::Freenect freenect;
-		//	MyFreenectDevice& device = freenect.createDevice<MyFreenectDevice>(0);
-
-		//	namedWindow("rgb",CV_WINDOW_AUTOSIZE);
-		//	namedWindow("depth",CV_WINDOW_AUTOSIZE);
-
-		//	device.startVideo();
-		//	device.startDepth();
-		}
+		CamThread();
 
 		~CamThread();
-
-
-		// Do not call directly even in child
-		void VideoCallback(void* _rgb, uint32_t timestamp);
-
-		// Do not call directly even in child
-		void DepthCallback(void* _depth, uint32_t timestamp);
-
-		bool getVideo(Mat& output);
-
-		bool getDepth(Mat& output);
 
 		/**
 		@return true on success
@@ -172,9 +124,6 @@ class CamThread : public QThread, public Freenect::FreenectDevice
 
 
 	private:
-		std::vector<uint8_t> m_buffer_depth;
-		std::vector<uint8_t> m_buffer_rgb;
-		std::vector<uint16_t> m_gamma;
 		mutable QMutex m_rgb_mutex;
 		mutable QMutex m_depth_mutex;
 		Mat depthMat;
