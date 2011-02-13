f58a1368496cc4139742231794549dc9e5bc5fe3
markusk <github@direcs.de>
2011-02-13 17:28:08 +0100

    Command TURNRIGHT fixed due to new wiring on the bot

--- direcs-avr/main.c
+++ direcs-avr/main.c
@@ -890,16 +890,19 @@ int main(void)
 				PORTL &= ~(1<<PIN0);
 				// set Motor1 B bit
 				PORTL |= (1<<PIN1);
+				
 				// MOTOR 2 COUNTERCLOCKWISE
 				// set Motor2 A bit
 				PORTL |= (1<<PIN2);
 				// delete Motor2 B bit
 				PORTL &= ~(1<<PIN3);
+				
 				// MOTOR 3 CLOCKWISE
 				// delete Motor3 A bit
 				PORTL &= ~(1<<PIN6);
 				// set Motor3 B bit
 				PORTL |= (1<<PIN7);
+				
 				// MOTOR 4 COUNTERCLOCKWISE
 				// set Motor4 A bit
 				PORTD |= (1<<PIN6);
@@ -913,29 +916,29 @@ int main(void)
 			// TURNRIGHT = "bot turn right"
 			if (strcmp(stringbuffer, "*btr#") == 0)
 			{
-				// MOTOR 1 COUNTERCLOCKWISE -> changed to CLOCKWISE due to mounting on the bot!
+				// MOTOR 1 COUNTERCLOCKWISE = backward
 				// set Motor1 A bit
 				PORTL |= (1<<PIN0);
 				// delete Motor1 B bit
 				PORTL &= ~(1<<PIN1);
 
-				// MOTOR 2 CLOCKWISE -> changed to COUNTERCLOCKWISE due to mounting on the bot!
+				// MOTOR 2 CLOCKWISE = forward
 				// delete Motor2 A bit
 				PORTL &= ~(1<<PIN2);
 				// set Motor2 B bit
 				PORTL |= (1<<PIN3);
 
-				// MOTOR 3 COUNTERCLOCKWISE
+				// MOTOR 3 COUNTERCLOCKWISE = backward
 				// set Motor3 A bit
 				PORTL |= (1<<PIN6);
 				// delete Motor3 B bit
 				PORTL &= ~(1<<PIN7);
 
-				// MOTOR 4 CLOCKWISE -> changed to COUNTERCLOCKWISE due to mounting on the bot!
-				// set Motor4 A bit
-				PORTD |= (1<<PIN6);
-				// delete Motor4 B bit
-				PORTD &= ~(1<<PIN7);
+				// MOTOR 4 CLOCKWISE = forward
+				// delete Motor4 A bit
+				PORTD &= ~(1<<PIN6);
+				// set Motor4 B bit
+				PORTD |= (1<<PIN7);
 
 				// answer with "ok"
 				put_string("*btr#");
