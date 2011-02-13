6b4c1d635eb34ec037b4845513dcd79803f2575f
markusk <github@direcs.de>
2011-02-13 18:09:14 +0100

    Command BOTBACKWARD fixed due to new wiring on the bot

--- direcs-avr/main.c
+++ direcs-avr/main.c
@@ -801,26 +801,29 @@ int main(void)
 			// BOTBACKWARD = "bot drive backward"
 			if (strcmp(stringbuffer, "*bdb#") == 0)
 			{
-				// MOTOR 1 COUNTERCLOCKWISE
+				// MOTOR 1 COUNTERCLOCKWISE = backward
 				// set Motor1 A bit
 				PORTL |= (1<<PIN0);
 				// delete Motor1 B bit
 				PORTL &= ~(1<<PIN1);
-				// MOTOR 2 CLOCKWISE
-				// delete Motor2 A bit
-				PORTL &= ~(1<<PIN2);
-				// set Motor2 B bit
-				PORTL |= (1<<PIN3);
-				// MOTOR 3 COUNTERCLOCKWISE
+
+				// MOTOR 2 COUNTERCLOCKWISE = backward
+				// set Motor2 A bit
+				PORTL |= (1<<PIN2);
+				// delete Motor2 B bit
+				PORTL &= ~(1<<PIN3);
+
+				// MOTOR 3 COUNTERCLOCKWISE = backward
 				// set Motor3 A bit
 				PORTL |= (1<<PIN6);
 				// delete Motor3 B bit
 				PORTL &= ~(1<<PIN7);
-				// MOTOR 4 CLOCKWISE
-				// delete Motor4 A bit
-				PORTD &= ~(1<<PIN6);
-				// set Motor4 B bit
-				PORTD |= (1<<PIN7);
+
+				// MOTOR 4 COUNTERCLOCKWISE = backward
+				// set Motor4 A bit
+				PORTD |= (1<<PIN6);
+				// delete Motor4 B bit
+				PORTD &= ~(1<<PIN7);
 
 				// answer with "ok"
 				put_string("*bdb#");
