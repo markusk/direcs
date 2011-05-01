35ab72d4f3ca5bcf02e15daf129dd384c459b04f
markusk <github@direcs.de>
2011-05-01 11:58:39 +0200

    Removed obsolete #defines for the old serial commands

--- direcs-avrsim/src/simulationThread.cpp
+++ direcs-avrsim/src/simulationThread.cpp
@@ -67,7 +67,7 @@ void SimulationThread::run()
 			//---------------------------------------------------------
 			// Lock the mutex. If another thread has locked the mutex then this call will block until that thread has unlocked it.
 			mutex->lock();
-			
+
 			if (interface1->receiveChar(&receivedChar) == false)
 			{
 				// Unlock the mutex.
@@ -81,10 +81,10 @@ void SimulationThread::run()
 				mutex->unlock();
 				switch (receivedChar)
 				{
-					case INIT:
+					case 64:
 						// "answer" with "@" [Ascii Dezimal @ = 64]
 						// this answer is used to see if the robot is "on"
-						if (interface1->sendChar(INIT) == false)
+						if (interface1->sendChar(64) == false)
 						{
 							// Unlock the mutex.
 							mutex->unlock();
@@ -113,19 +113,19 @@ void SimulationThread::run()
 					PORTD &= ~(1<<PIN7);
 					// flashlight off
 					PORTC &= ~(1<<PIN1);
-					
+
 					// 				setServoPosition(1, 17); // <- exact position now in the mrs.ini!
 					// 				setServoPosition(2, 19); // <- exact position now in the mrs.ini!
 					// 				setServoPosition(3, 23); // <- exact position now in the mrs.ini!
 					// 				setServoPosition(4, 19); // <- exact position now in the mrs.ini!
 					// 				setServoPosition(5, 19); // <- exact position now in the mrs.ini!
 					// 				setServoPosition(6, 22); // <- exact position now in the mrs.ini!
-					
+
 					// "answer" with "@" [Ascii Dezimal @ = 64]
 					// this answer is used to see if the robot is "on"
 					UsartTransmit( (uint8_t)(64) );
 					break;
-					
+
 					//-------------------------------
 				case READ_SENSOR_1:
 					// read value from the analog digital converter (ADC)
@@ -135,7 +135,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_SENSOR_2:
 					// read value from the analog digital converter (ADC)
 					value = readADC(SENSOR2);
@@ -144,7 +144,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_SENSOR_3:
 					// read value from the analog digital converter (ADC)
 					value = readADC(SENSOR3);
@@ -153,7 +153,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_SENSOR_4:
 					// read value from the analog digital converter (ADC)
 					value = readADC(SENSOR4);
@@ -162,7 +162,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_SENSOR_5:
 					// read value from the analog digital converter (ADC)
 					value = readADC(SENSOR5);
@@ -171,7 +171,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_SENSOR_6:
 					// read value from the analog digital converter (ADC)
 					value = readADC(SENSOR6);
@@ -180,7 +180,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_SENSOR_7:
 					// read value from the analog digital converter (ADC)
 					value = readADC(SENSOR7);
@@ -189,7 +189,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_SENSOR_8:
 					// read value from the analog digital converter (ADC)
 					value = readADC(SENSOR8);
@@ -198,7 +198,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_SENSOR_16:
 					// ultra sonic sensor !!
 					value = ultraschall_messung();
@@ -207,7 +207,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_MOTOR_SENSOR1:
 					// motor sensor
 					value = readADC(SENSORMOTOR1);
@@ -216,7 +216,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_MOTOR_SENSOR2:
 					// motor sensor
 					value = readADC(SENSORMOTOR2);
@@ -225,7 +225,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_MOTOR_DISTANCE1:
 					// driven distance of motor 1 (encoder)
 					value = rightDistanceCounter;
@@ -234,7 +234,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_MOTOR_DISTANCE2:
 					// driven distance of motor 2 (encoder)
 					value = leftDistanceCounter;
@@ -243,19 +243,19 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case RESET_MOTOR_DISTANCE1:
 					// reset driven distance of motor 1 (encoder)
 					rightDistanceCounter = 0;
 					rightWheelCounter = 0;
 					break;
-					
+
 				case RESET_MOTOR_DISTANCE2:
 					// reset driven distance of motor 1 (encoder)
 					leftDistanceCounter = 0;
 					leftWheelCounter = 0;
 					break;
-					
+
 					//-------------------------------
 				case MOTOR1_OFF:
 					// delete Motor1 A bit
@@ -263,27 +263,27 @@ void SimulationThread::run()
 					// delete Motor1 B bit
 					PORTL &= ~(1<<PIN1);
 					break;
-					
+
 				case MOTOR1_CLOCKWISE:
 					// delete Motor1 A bit
 					PORTL &= ~(1<<PIN0);
 					// set Motor1 B bit
 					PORTL |= (1<<PIN1);
 					break;
-					
+
 				case MOTOR1_COUNTERCLOCKWISE:
 					// set Motor1 A bit
 					PORTL |= (1<<PIN0);
 					// delete Motor1 B bit
 					PORTL &= ~(1<<PIN1);
 					break;
-					
+
 				case MOTOR1_SPEED_SET:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setPWMwidth(1, value);
 					break;
-					
+
 					//-------------------------------
 				case MOTOR2_OFF:
 					// delete Motor2 A bit
@@ -291,27 +291,27 @@ void SimulationThread::run()
 					// delete Motor2 B bit
 					PORTL &= ~(1<<PIN3);
 					break;
-					
+
 				case MOTOR2_CLOCKWISE:
 					// delete Motor2 A bit
 					PORTL &= ~(1<<PIN2);
 					// set Motor2 B bit
 					PORTL |= (1<<PIN3);
 					break;
-					
+
 				case MOTOR2_COUNTERCLOCKWISE:
 					// set Motor2 A bit
 					PORTL |= (1<<PIN2);
 					// delete Motor2 B bit
 					PORTL &= ~(1<<PIN3);
 					break;
-					
+
 				case MOTOR2_SPEED_SET:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setPWMwidth(2, value);
 					break;
-					
+
 					//-------------------------------
 				case MOTOR3_OFF:
 					// delete Motor3 A bit
@@ -319,7 +319,7 @@ void SimulationThread::run()
 					// delete Motor3 B bit
 					PORTL &= ~(1<<PIN7);
 					break;
-					
+
 				case MOTOR3_CLOCKWISE: // cam pan R
 					// only, when end switch is clear
 					if ( bit_is_clear(PINK,PIN2) )
@@ -330,7 +330,7 @@ void SimulationThread::run()
 						PORTL |= (1<<PIN7);
 					}
 					break;
-					
+
 				case MOTOR3_COUNTERCLOCKWISE: // cam pan L
 					// only, when end switch is clear
 					if ( bit_is_clear(PINK,PIN3) )
@@ -341,13 +341,13 @@ void SimulationThread::run()
 						PORTL &= ~(1<<PIN7);
 					}
 					break;
-					
+
 				case MOTOR3_SPEED_SET:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setPWMwidth(3, value);
 					break;
-					
+
 					//-------------------------------
 				case MOTOR4_OFF:
 					// delete Motor4 A bit
@@ -355,7 +355,7 @@ void SimulationThread::run()
 					// delete Motor4 B bit
 					PORTD &= ~(1<<PIN7);
 					break;
-					
+
 				case MOTOR4_CLOCKWISE: // cam tilt top
 					// only, when end switch is clear
 					if ( bit_is_clear(PINK,PIN1) )
@@ -366,7 +366,7 @@ void SimulationThread::run()
 						PORTD |= (1<<PIN7);
 					}
 					break;
-					
+
 				case MOTOR4_COUNTERCLOCKWISE: // cam tilt bottom
 					if ( bit_is_clear(PINK,PIN0) )
 					{
@@ -376,53 +376,53 @@ void SimulationThread::run()
 						PORTD &= ~(1<<PIN7);
 					}
 					break;
-					
+
 				case MOTOR4_SPEED_SET:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setPWMwidth(4, value);
 					break;
-					
+
 					//-------------------------------
 				case SET_SERVO1:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setServoPosition(1, value);
 					break;
-					
+
 				case SET_SERVO2:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setServoPosition(2, value);
 					break;
-					
+
 				case SET_SERVO3:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setServoPosition(3, value);
 					break;
-					
-					
+
+
 				case SET_SERVO4:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setServoPosition(4, value);
 					break;
-					
-					
+
+
 				case SET_SERVO5:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setServoPosition(5, value);
 					break;
-					
-					
+
+
 				case SET_SERVO6:
 					// wait for the (second) value to set the pwm!
 					value = UsartReceive();
 					setServoPosition(6, value);
 					break;
-					
+
 					//-------------------------------
 				case FLASHLIGHT_ON:
 					// set flashlight bit
@@ -434,32 +434,32 @@ void SimulationThread::run()
 					PORTC &= ~(1<<PIN1);
 					PORTC |= (1<<PIN0); // < yellow led
 					break;
-					
+
 					//-------------------------------
 				case READ_CONTACT1:
 					// contact cam tilt R/BOTTOM
 					// send 1 Byte (8 bit!)
 					UsartTransmit( (uint8_t) bit_is_set(PINK,PIN3) );
 					break;
-					
+
 				case READ_CONTACT2:
 					// contact cam tilt L/TOP
 					// send 1 Byte (8 bit!)
 					UsartTransmit( (uint8_t) bit_is_set(PINK,PIN2) );
 					break;
-					
+
 				case READ_CONTACT3:
 					// contact cam pan R
 					// send 1 Byte (8 bit!)
 					UsartTransmit( (uint8_t) bit_is_set(PINK,PIN1) );
 					break;
-					
+
 				case READ_CONTACT4:
 					// contact cam pan L
 					// send 1 Byte (8 bit!)
 					UsartTransmit( (uint8_t) bit_is_set(PINK,PIN0) );
 					break;
-					
+
 				case READ_AXIS_X:
 					// read x axis from the micromag3 sensor
 					value = readMicromag(READ_AXIS_X);
@@ -468,7 +468,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_AXIS_Y:
 					// read x axis from the micromag3 sensor
 					value = readMicromag(READ_AXIS_Y);
@@ -477,7 +477,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 				case READ_AXIS_Z:
 					// read x axis from the micromag3 sensor
 					value = readMicromag(READ_AXIS_Z);
@@ -486,7 +486,7 @@ void SimulationThread::run()
 					// send LS-Byte
 					UsartTransmit( (uint8_t)(value) );
 					break;
-					
+
 	// 			default:
 	// 				//
 	// 				// yellow 'traffic' LED
@@ -504,7 +504,7 @@ void SimulationThread::run()
 	// 					PORTC |= (1<<PIN0);
 	// 				}
 	// 				break;
-							
+
 			} // switch
 			//====================================================================
 			//====================================================================
@@ -517,14 +517,14 @@ void SimulationThread::run()
 			mutex->unlock();
 
 		} // simulation = false
-		
+
 		if (simulationMode)
 		{
 			// FIXME: here or where, when simulating?????  send an optical heartbeat signal to the GUI
 			// TODO: now we're sleeping 500ms because ww're only simulating. Is this okay????
 			msleep(500);
 			heartbeatToggle = !heartbeatToggle;
-			
+
 			emit heartbeat(heartbeatToggle);
 		}
 
