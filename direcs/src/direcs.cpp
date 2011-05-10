f720d3386b258b082bac1860660868bafba40d60
markusk <github@direcs.de>
2011-05-10 20:38:51 +0200

    Typo fixed in log message.

--- direcs/src/direcs.cpp
+++ direcs/src/direcs.cpp
@@ -697,7 +697,7 @@ void Direcs::init()
 			else
 			{
 				logfile->appendLog("Robot is OFF! Please turn it ON!");
-				emit message("cThe robot is OFF! Please turn it ON!</font>");
+				emit message("<font color=\"#FF0000\">The robot is OFF! Please turn it ON!</font>");
 				emit message("Heartbeat thread NOT started!");
 				emit message("Sensor thread NOT started!");
 				emit message("Plot thread NOT started!");
