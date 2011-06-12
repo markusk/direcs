c795acc1564d72a747815944a3c3965533accfc7
markusk <github@direcs.de>
2011-06-12 18:48:45 +0200

    Set thread sleep time to 750 ms. To do: check with robot!

--- direcs/src/sensorThread.h
+++ direcs/src/sensorThread.h
@@ -317,7 +317,7 @@ class SensorThread : public QThread
 
 		// Every thread sleeps some time, for having a bit more time fo the other threads!
 		// Time in milliseconds
-		static const unsigned long THREADSLEEPTIME = 1000; // Default: 100 ms
+		static const unsigned long THREADSLEEPTIME = 750; // Default: 100 ms
 
 		// Give the infrared sensors some names
 		//
