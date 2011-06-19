816b992c5da3b13f1e0d70220e38fc47576f276a
markusk <github@direcs.de>
2011-06-19 23:48:04 +0200

    Fixed head inclusion

--- test/src/lightweightserial.cpp
+++ test/src/lightweightserial.cpp
@@ -27,7 +27,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include "serial_port/lightweightserial.h"
+#include "lightweightserial.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
