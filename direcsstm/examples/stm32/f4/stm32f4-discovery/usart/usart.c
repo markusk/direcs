f9cbdf62d0aec8b28a6206f141f6b5e3844531bb
markusk <github@direcs.de>
2012-01-06 18:30:13 +0100

    Updated comments, changed order of 8N1 setting

--- direcsstm/examples/stm32/f4/stm32f4-discovery/usart/usart.c
+++ direcsstm/examples/stm32/f4/stm32f4-discovery/usart/usart.c
@@ -34,27 +34,30 @@ void clock_setup(void)
 
 void usart_setup(void)
 {
-	/* Setup USART2 parameters. */
+	// Setup USART2 parameters
+
+	// 38400
 	usart_set_baudrate(USART2, 38400);
+
+	// 8N1
 	usart_set_databits(USART2, 8);
-	usart_set_stopbits(USART2, USART_STOPBITS_1);
-	usart_set_mode(USART2, USART_MODE_TX);
 	usart_set_parity(USART2, USART_PARITY_NONE);
 	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
 
-	/* Finally enable the USART. */
+	// Finally enable the USART.
 	usart_enable(USART2);
 }
 
 void gpio_setup(void)
 {
-	/* Setup GPIO pin GPIO12 on GPIO port D for LED. */
+	// Setup GPIO pin GPIO12 on GPIO port D for LED.
 	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
 
-	/* Setup GPIO pins for USART2 transmit. */
+	// Setup GPIO pins for USART2 transmit.
+	// TX
 	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
 
-	/* Setup USART2 TX pin as alternate function. */
+	// Setup USART2 TX pin as alternate function.
 	gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
 }
 
