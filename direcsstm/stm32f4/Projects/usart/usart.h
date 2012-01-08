0d84eda08e977de91cddd6c4efae5bfd33c0231c
markusk <github@direcs.de>
2012-01-08 16:40:27 +0100

    Removed AVR specific stuff

--- direcsstm/stm32f4/Projects/usart/usart.h
+++ direcsstm/stm32f4/Projects/usart/usart.h
@@ -1,20 +1,7 @@
 #ifndef USART_H
 #define USART_H
 
-#ifndef F_CPU
-#define F_CPU 16000000
-#endif
 
-#ifndef USART_BAUD_RATE
-#define USART_BAUD_RATE 9600
-#endif
-
-#define USART_BAUD_SELECT (F_CPU / (USART_BAUD_RATE * 16L) - 1)
-#define CR 13
-#define LF 10
-
-
-#include <avr/io.h>
 #include <string.h>
 #include "main.h" // for LED methods
 
