e3fe932561455fef3a4d35f5626556d39acd78d4
markusk <github@direcs.de>
2012-01-08 16:42:55 +0100

    Replaced uart_buffer_size by UARTBUFFERSIZE

--- direcsstm/stm32f4/Projects/usart/usart.h
+++ direcsstm/stm32f4/Projects/usart/usart.h
@@ -7,7 +7,8 @@
 
 
 // Puffergrösse in Bytes, RX und TX sind gleich gross
-#define uart_buffer_size 32
+#define UARTBUFFERSIZE	32
+
 
 // some global variables for ISR routines
 // beachte: volatile damit Wert auch außerhalb der ISR gelesen werden kann! Wird sonst vom Compiler wegoptimiert.
@@ -17,8 +18,8 @@ volatile uint8_t TXcompleted; // Flag, String komplett gesendet
 volatile uint8_t starter;    // this marks the beginning of a received string. which is '*' at the moment.
 volatile uint8_t terminator; // this marks the end of a string. which is '#' at the moment.
 
-char uart_rx_buffer[uart_buffer_size+1]; // Empfangspuffer (+1 wg. zusätzlichem \0 in ISR RX)
-char uart_tx_buffer[uart_buffer_size+1]; // Sendepuffer    (+1 wg. zusätzlichem \0 in ISR RX)
+char uart_rx_buffer[UARTBUFFERSIZE + 1]; // Empfangspuffer (+1 wg. zusätzlichem \0 in ISR RX)
+char uart_tx_buffer[UARTBUFFERSIZE + 1]; // Sendepuffer    (+1 wg. zusätzlichem \0 in ISR RX)
 
 volatile uint8_t redLEDtoggle;
 volatile uint8_t greenLEDtoggle;
