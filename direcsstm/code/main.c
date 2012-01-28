a6b78ddb78c341980a0b92ec46a1c310be338e02
markusk <github@direcs.de>
2012-01-28 21:26:17 +0100

    Added port init for motor bits

--- direcsstm/code/main.c
+++ direcsstm/code/main.c
@@ -163,7 +163,7 @@ int main(void)
 	usartInit();
 
 	//	--------------------------------------------------------------------------------
-	//	GPIOPort init
+	//	GPIOPort init, Port D
 	// 	Don't know why, but this code has to be here (not in a seperate method)
 	//	--------------------------------------------------------------------------------
 	// GPIOD Periph clock enable
@@ -171,6 +171,22 @@ int main(void)
 
 	// Configure PD12, PD13, PD14 and PD15 in output pushpull mode
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	//	--------------------------------------------------------------------------------
+
+	//	--------------------------------------------------------------------------------
+	//	GPIOPort init, Port B
+	// 	Don't know why, but this code has to be here (not in a seperate method)
+	//	--------------------------------------------------------------------------------
+	// GPIOD Periph clock enable
+	RCC_AHB1PeriphClockCmd(MOTOR1PORT, ENABLE);
+
+	// Motor control Bits
+	GPIO_InitStructure.GPIO_Pin = MOTOR1BITA | MOTOR1BITB; // enable motor 1 A+B
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
