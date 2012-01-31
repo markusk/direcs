5b9946c4b0193cd2f8e92a52fac3b18ba9753798
markusk <github@direcs.de>
2012-01-31 23:00:29 +0100

    Removed PWM code for channel 1 and 2, updated ports and Pins. This should now work for Port C with Pin 8 and 9 (TIM3:CH3 and 4).

--- direcsstm/code/main.c
+++ direcsstm/code/main.c
@@ -229,40 +229,20 @@ int main(void)
 
 	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
 
-	/* PWM1 Mode configuration: Channel1 */
-	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
-	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-
-	TIM_OC1Init(TIM3, &TIM_OCInitStructure);
-
-	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-	/* PWM1 Mode configuration: Channel2 */
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
-
-	TIM_OC2Init(TIM3, &TIM_OCInitStructure);
-
-	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
 	/* PWM1 Mode configuration: Channel3 */
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_Pulse = CCR3_Val;
-
 	TIM_OC3Init(TIM3, &TIM_OCInitStructure);
-
 	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
 	/* PWM1 Mode configuration: Channel4 */
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
-
 	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
-
 	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
+	//
 	TIM_ARRPreloadConfig(TIM3, ENABLE);
 
 	/* TIM3 enable counter */
@@ -476,30 +456,20 @@ void TIM_Config(void)
   /* TIM3 clock enable */
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-  /* GPIOC and GPIOB clock enable */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOB, ENABLE);
+  /* GPIOC clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
   
-  /* GPIOC Configuration: TIM3 CH1 (PC6) and TIM3 CH2 (PC7) */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7 ;
+  /* GPIOC Configuration:  TIM3 CH3 (PC8) and TIM3 CH4 (PC9) */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
   GPIO_Init(GPIOC, &GPIO_InitStructure); 
-  
-  /* GPIOB Configuration:  TIM3 CH3 (PB0) and TIM3 CH4 (PB1) */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
-  GPIO_Init(GPIOB, &GPIO_InitStructure); 
 
   /* Connect TIM3 pins to AF2 */  
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_TIM3);
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_TIM3); 
-  GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_TIM3);
-  GPIO_PinAFConfig(GPIOB, GPIO_PinSource1, GPIO_AF_TIM3); 
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_TIM3);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_TIM3); 
 }
 
 
