b83483e04a8df0c5f2a19034524a3ef648ccf933
markusk <github@direcs.de>
2012-02-01 23:42:07 +0100

    Update PWM code to original source example from STM See also: TIM_PWM_Output dir Works fine for PC6 and 7 pins

--- direcsstm/code/main.c
+++ direcsstm/code/main.c
@@ -187,7 +187,7 @@ int main(void)
 	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	// call timer config for PWM
-	// TIM_Config();
+	TIM_Config();
 
 	/* -----------------------------------------------------------------------
     TIM3 Configuration: generate 4 PWM signals with 4 different duty cycles.
@@ -217,7 +217,7 @@ int main(void)
      function to update SystemCoreClock variable value. Otherwise, any configuration
      based on this variable will be incorrect.    
 	----------------------------------------------------------------------- */  
-/*
+
 	// Compute the prescaler value
 	PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 28000000) - 1;
 
@@ -229,28 +229,47 @@ int main(void)
 
 	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
 
-
-	// PWM1 Mode configuration: Channel3
-	// GPIOC Configuration:  TIM3 CH3 (PC8) and TIM3 CH4 (PC9)
+	// PWM1 Mode configuration: Channel1
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+
+	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+	// PWM1 Mode configuration: Channel2
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
+
+	TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+
+	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+	// PWM1 Mode configuration: Channel3
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = CCR3_Val;
+
 	TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+
 	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
 	// PWM1 Mode configuration: Channel4
-	// GPIOC Configuration:  TIM3 CH3 (PC8) and TIM3 CH4 (PC9)
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
+
 	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
+
 	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
-	//
 	TIM_ARRPreloadConfig(TIM3, ENABLE);
 
 	// TIM3 enable counter
 	TIM_Cmd(TIM3, ENABLE);
 
 	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 
 	//	--------------------------------------------------------------------------------
@@ -292,113 +311,7 @@ int main(void)
 	turnLED(LEDRED, OFF);
 	turnLED(LEDBLUE, OFF);
 
-/*
-	// Motor bits off
-	// @todo !!
-while(1)
-{
-	TIM_Cmd(TIM3, DISABLE);
-		// Time base configuration
-	TIM_TimeBaseStructure.TIM_Period = 665;
-	TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
-	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-//-
-	CCR4_Val = 333;
-	// PWM1 Mode configuration: Channel4
-	// GPIOC Configuration:  TIM3 CH3 (PC8) and TIM3 CH4 (PC9)
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
-	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
-	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-//-
-	TIM_ARRPreloadConfig(TIM3, ENABLE);
-	TIM_Cmd(TIM3, ENABLE);
-
-	turnLED(LEDBLUE, ON);
-
-	// Insert delay
-	Delay(0x3FFFFF);
-
-	TIM_Cmd(TIM3, DISABLE);
-		// Time base configuration
-	TIM_TimeBaseStructure.TIM_Period = 665;
-	TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
-	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-//-
-	CCR4_Val = 249;
-	// PWM1 Mode configuration: Channel4
-	// GPIOC Configuration:  TIM3 CH3 (PC8) and TIM3 CH4 (PC9)
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
-	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
-	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-//-
-	TIM_ARRPreloadConfig(TIM3, ENABLE);
-	TIM_Cmd(TIM3, ENABLE);
-
-	turnLED(LEDBLUE, OFF);
-
-	// Insert delay
-	Delay(0x3FFFFF);
-
-	TIM_Cmd(TIM3, DISABLE);
-		// Time base configuration
-	TIM_TimeBaseStructure.TIM_Period = 665;
-	TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
-	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-//-
-	CCR4_Val = 166;
-	// PWM1 Mode configuration: Channel4
-	// GPIOC Configuration:  TIM3 CH3 (PC8) and TIM3 CH4 (PC9)
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
-	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
-	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-//-
-	TIM_ARRPreloadConfig(TIM3, ENABLE);
-	TIM_Cmd(TIM3, ENABLE);
-
-	turnLED(LEDBLUE, ON);
-
-	// Insert delay
-	Delay(0x3FFFFF);
-
-	TIM_Cmd(TIM3, DISABLE);
-		// Time base configuration
-	TIM_TimeBaseStructure.TIM_Period = 665;
-	TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
-	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-//-
-	CCR4_Val = 83;
-	// PWM1 Mode configuration: Channel4
-	// GPIOC Configuration:  TIM3 CH3 (PC8) and TIM3 CH4 (PC9)
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
-	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
-	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-//-
-	TIM_ARRPreloadConfig(TIM3, ENABLE);
-	TIM_Cmd(TIM3, ENABLE);
-
-	turnLED(LEDBLUE, OFF);
-
-	// Insert delay
-	Delay(0x3FFFFF);
-
-}
-*/
 	while (1)
 	{
 //		serialBlockingReadString();
@@ -555,25 +468,35 @@ void gpioPortInit()
   */
 void TIM_Config(void)
 {
-  GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
 
-  /* TIM3 clock enable */
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+	// TIM3 clock enable
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-  /* GPIOC clock enable */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-  
-  /* GPIOC Configuration:  TIM3 CH3 (PC8) and TIM3 CH4 (PC9) */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
-  GPIO_Init(GPIOC, &GPIO_InitStructure); 
+	// GPIOC and GPIOB clock enable
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOB, ENABLE);
 
-  /* Connect TIM3 pins to AF2 */  
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_TIM3);
-  GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_TIM3); 
+	// GPIOC Configuration: TIM3 CH1 (PC6) and TIM3 CH2 (PC7)
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7 ;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+	GPIO_Init(GPIOC, &GPIO_InitStructure); 
+
+	// GPIOB Configuration:  TIM3 CH3 (PB0) and TIM3 CH4 (PB1)
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+	GPIO_Init(GPIOB, &GPIO_InitStructure); 
+
+	// Connect TIM3 pins to AF2  
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_TIM3);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_TIM3); 
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_TIM3);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource1, GPIO_AF_TIM3); 
 }
 
 
