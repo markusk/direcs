a249807fc788bcc30df24999d2c6ad2b6085ccdf
markusk <github@direcs.de>
2012-02-13 17:29:35 +0100

    Added atoi method and stdlib.h inclusion

--- direcsstm/code/main.c
+++ direcsstm/code/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "stm32f4xx_conf.h"
 
 //
@@ -156,6 +157,7 @@ void turnLED(int led, int state);
 void sendUInt(uint16_t value);
 
 char* ltoa(long value, char* result);
+int atoi(const char *s);
 
 void Delay(__IO uint32_t nCount);
 
@@ -635,6 +637,66 @@ void Delay(__IO uint32_t nCount)
 	{
 	}
 }
+
+
+int atoi(const char *s)
+{
+	static const char digits[] = "0123456789";  /* legal digits in order */
+	unsigned val=0;         /* value we're accumulating */
+	int neg=0;              /* set to true if we see a minus sign */
+
+	/* skip whitespace */
+	while (*s==' ' || *s=='\t')
+	{
+		s++;
+	}
+
+	/* check for sign */
+	if (*s=='-')
+	{
+		neg=1;
+		s++;
+	}
+	else if (*s=='+')
+	{
+		s++;
+	}
+
+	/* process each digit */
+	while (*s)
+	{
+		const char *where;
+		unsigned digit;
+
+		/* look for the digit in the list of digits */
+		where = strchr(digits, *s);
+		if (where==NULL)
+		{
+			/* not found; not a digit, so stop */
+			break;
+		}
+
+		/* get the index into the digit list, which is the value */
+		digit = (where - digits);
+
+		/* could (should?) check for overflow here */
+
+		/* shift the number over and add in the new digit */
+		val = val*10 + digit;
+
+		/* look at the next character */
+		s++;
+	}
+
+	/* handle negative numbers */
+	if (neg)
+	{
+		return -val;
+	}
+
+	/* done */
+	return val;
+ }
 
 
 #ifdef  USE_FULL_ASSERT
