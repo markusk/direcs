e48476bdbb4b5b8795faed06142ab2319a5825e2
markusk <github@direcs.de>
2012-03-11 19:42:26 +0100

    Bug fixed: updated flashlight call in resetRobot

--- direcsstm/code/main.c
+++ direcsstm/code/main.c
@@ -631,7 +631,7 @@ void resetRobot(void)
 	turnLED(LEDBLUE, OFF);
 
 	// flashing light off
-	flashlight(FLASHLIGHT, OFF);
+	flashlight(OFF);
 
 	// turn all drive motor bits off (except PWM bits)
 	GPIO_ResetBits(MOTORPORT, MOTOR1BITA | MOTOR1BITB | MOTOR2BITA | MOTOR2BITB | MOTOR3BITA | MOTOR3BITB | MOTOR4BITA | MOTOR4BITB);
