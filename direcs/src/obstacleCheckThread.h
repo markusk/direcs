252a0e38d9bc1cc10fb4c6edad0085dfcc66b205
markusk <github@direcs.de>
2012-08-03 22:54:40 +0200

    Set ms to 10ms in obstacleCheckThread

--- direcs/src/obstacleCheckThread.h
+++ direcs/src/obstacleCheckThread.h
@@ -204,7 +204,7 @@ class ObstacleCheckThread : public QThread
 
 		// Every thread sleeps some time, for having a bit more time fo the other threads!
 		// Time in milliseconds
-		static const unsigned long THREADSLEEPTIME = 1000; // 250 ! original !
+		static const unsigned long THREADSLEEPTIME = 10; // 250 ! original !
 
 		// Give the sensors some names
 		//
