44e8d8f988863bc4a5807a346fff3bcd6bf86aac
Markus Knapp <github@direcs.de>
2012-09-30 19:28:28 +0200

    Added lcdinit code from other file

--- Touch-LCD/watterott/MI0283QT-2.c
+++ Touch-LCD/watterott/MI0283QT-2.c
@@ -11,10 +11,24 @@
 #define LCD_DATA             ((0x72)|(LCD_ID<<2))
 #define LCD_REGISTER         ((0x70)|(LCD_ID<<2))
 
-#define LCD_RST_DISABLE()    RST high
-#define LCD_RST_ENABLE()     RST low
-#define LCD_CS_DISABLE()     CS high
-#define LCD_CS_ENABLE()      CS low
+
+//#define LCD_RST_DISABLE()    RST high
+//#define LCD_RST_ENABLE()     RST low
+//#define LCD_CS_DISABLE()     CS high
+//#define LCD_CS_ENABLE()      CS low
+
+// - - - - - 
+#define LED_PIN (1 << 6)
+#define LCD_CS_PIN (1 << 7)
+#define LCD_RESET_PIN (1 << 8)
+
+#define LCD_RST_DISABLE()    GPIOB->BSRRL = LCD_RESET_PIN
+#define LCD_RST_ENABLE()     GPIOB->BSRRH = LCD_RESET_PIN
+#define LCD_CS_DISABLE()     GPIOB->BSRRL = LCD_CS_PIN
+#define LCD_CS_ENABLE()      GPIOB->BSRRH = LCD_CS_PIN
+#define LED_ENABLE()         GPIOB->BSRRL = LED_PIN
+#define LED_DISABLE()        GPIOB->BSRRH = LED_PIN
+// - - - - -
 
 
 void lcd_clear(unsigned int color)
@@ -191,4 +205,91 @@ void lcd_reset(void)
   lcd_area(0, 0, (LCD_WIDTH-1), (LCD_HEIGHT-1));
 
   return;
+}
+
+void lcdInit()
+{
+   // Setup SPI
+#ifdef USE_STDPERIPH_DRV
+
+   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+   GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_SPI1);
+   GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_SPI1);
+   GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_SPI1);
+
+   // LCD_CS_PIN / LCD_RESET_PIN / LED_PIN
+   GPIO_InitTypeDef lcdGpio;
+   lcdGpio.GPIO_Mode = GPIO_Mode_OUT;
+   lcdGpio.GPIO_OType = GPIO_OType_PP;
+   lcdGpio.GPIO_Pin = LCD_RESET_PIN | LED_PIN | LCD_CS_PIN;
+   lcdGpio.GPIO_PuPd = GPIO_PuPd_DOWN;
+   lcdGpio.GPIO_Speed = GPIO_Speed_100MHz;
+
+   GPIO_Init(GPIOB, &lcdGpio);
+
+   // SCK (PB3) / MISO (PB4) / MOSI (PB5)
+   GPIO_InitTypeDef spiGpio;
+   spiGpio.GPIO_Mode = GPIO_Mode_AF;
+   spiGpio.GPIO_OType = GPIO_OType_PP;
+   spiGpio.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5;
+   spiGpio.GPIO_PuPd = GPIO_PuPd_DOWN;
+   spiGpio.GPIO_Speed = GPIO_Speed_100MHz;
+
+   GPIO_Init(GPIOB, &spiGpio);
+
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+
+   SPI_InitTypeDef spiInit;
+   spiInit.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+   spiInit.SPI_DataSize = SPI_DataSize_8b;
+   spiInit.SPI_CPOL = SPI_CPOL_High;
+   spiInit.SPI_CPHA = SPI_CPHA_2Edge;
+   spiInit.SPI_NSS = SPI_NSS_Soft;
+   spiInit.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4; // 21 Mhz Takt
+   spiInit.SPI_FirstBit = SPI_FirstBit_MSB;
+   spiInit.SPI_CRCPolynomial = 7;
+   spiInit.SPI_Mode = SPI_Mode_Master;
+
+   SPI_Init(SPI1, &spiInit);
+
+   SPI_Cmd(SPI1, ENABLE);
+#else
+
+#endif
+
+   lcd_reset();
+
+   LED_ENABLE();
+
+   int delay = 500;
+
+   for (;;)
+   {
+      delay_ms(delay);
+      lcd_clear(COLOR_BLACK);
+      delay_ms(delay);
+      lcd_clear(COLOR_RED);
+      delay_ms(delay);
+      lcd_clear(COLOR_GREEN);
+      delay_ms(delay);
+      lcd_clear(COLOR_BLUE);
+      delay_ms(delay);
+      lcd_clear(COLOR_YELLOW);
+      delay_ms(delay);
+      lcd_clear(COLOR_WHITE);
+      delay_ms(delay);
+      lcd_clear(COLOR_BLACK);
+      delay_ms(3000);
+   }
+}
+
+#define spi_wait() while (SPI1->SR & SPI_SR_BSY)
+
+static inline void spi_write(uint8_t byte)
+{
+   SPI1->DR = byte;
+
+   // Loop while DR register in not empty
+   while (! (SPI1->SR & SPI_SR_TXE));
 }
