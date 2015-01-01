f2010fb61c473920cac7bd859fb65d2fea451d45
Markus Knapp <bitbucket@direcs.de>
2015-01-01 17:13:30 +0100

    Removed unused code (i.e. irsend). Reformatted code, too.

--- direcs-arduino/IRrecord/IRrecord.ino
+++ direcs-arduino/IRrecord/IRrecord.ino
@@ -1,23 +1,5 @@
-/*
- * IRrecord: record and play back IR signals as a minimal 
- * An IR detector/demodulator must be connected to the input RECV_PIN.
- * An IR LED must be connected to the output PWM pin 3.
- * A button must be connected to the input BUTTON_PIN; this is the
- * send button.
- * A visible LED can be connected to STATUS_PIN to provide status.
- *
- * The logic is:
- * If the button is pressed, send the IR code.
- * If an IR code is received, record it.
- *
- * Version 0.11 September, 2009
- * Copyright 2009 Ken Shirriff
- * http://arcfn.com
- */
-
 #include <IRremote.h>
 
-/*
 // Werte, die die Apple Remote "runder Donut, Alu" sendet
 unsigned long oben = 2011287637;
 unsigned long unten = 2011279445;
@@ -27,8 +9,8 @@ unsigned long ok = 2011282005;
 unsigned long menu = 2011250773;
 unsigned long play = 2011265621;
 unsigned long repeat = 2011242581; // Wird bei ok und play zusätzlich gesendet
-*/
 
+/*
 // Werte, die die Apple Remote "flacher Donut, Alu" sendet
 unsigned long oben = 2011287644;
 unsigned long unten = 2011279452;
@@ -38,54 +20,56 @@ unsigned long ok = 2011282012;
 unsigned long menu = 2011250780;
 unsigned long play = 2011265628;
 unsigned long repeat = 2011242588; // Wird bei ok und play zusätzlich gesendet
-
+*/
 
 int RECV_PIN = 2;
-int BUTTON_PIN = 12;
 int STATUS_PIN = 13;
 
 IRrecv irrecv(RECV_PIN);
-IRsend irsend;
 
 decode_results results;
+
 
 void setup()
 {
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
-
-/*
-  pinMode(BUTTON_PIN, INPUT);
-  pinMode(STATUS_PIN, OUTPUT);
-*/
 }
 
+
 // Storage for the recorded code
-int codeType = -1; // The type of code
-unsigned long codeValue; // The code value if not raw
-unsigned int rawCodes[RAWBUF]; // The durations if raw
-int codeLen; // The length of the code
-int toggle = 0; // The RC5/6 toggle state
+         int  codeType = -1;     // The type of code
+unsigned long codeValue;         // The code value if not raw
+unsigned int  rawCodes[RAWBUF];  // The durations if raw
+         int  codeLen;           // The length of the code
+         int toggle = 0;         // The RC5/6 toggle state
+
 
 // Stores the code for later playback
 // Most of this code is just logging
-void storeCode(decode_results *results) {
+void storeCode(decode_results *results)
+{
   codeType = results->decode_type;
   int count = results->rawlen;
-  if (codeType == UNKNOWN) {
+  
+  if (codeType == UNKNOWN)
+  {
     Serial.println("Received unknown code, saving as raw");
     codeLen = results->rawlen - 1;
     // To store raw codes:
     // Drop first value (gap)
     // Convert from ticks to microseconds
     // Tweak marks shorter, and spaces longer to cancel out IR receiver distortion
-    for (int i = 1; i <= codeLen; i++) {
-      if (i % 2) {
+    for (int i = 1; i <= codeLen; i++)
+    {
+      if (i % 2)
+      {
         // Mark
         rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK - MARK_EXCESS;
         Serial.print(" m");
       } 
-      else {
+      else
+      {
         // Space
         rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK + MARK_EXCESS;
         Serial.print(" s");
@@ -94,109 +78,72 @@ void storeCode(decode_results *results) {
     }
     Serial.println("");
   }
-  else {
-    if (codeType == NEC) {
+  else
+  {
+    if (codeType == NEC)
+    {
       Serial.print("Received NEC: ");
-      if (results->value == REPEAT) {
+      if (results->value == REPEAT) 
+      {
         // Don't record a NEC repeat value as that's useless.
         Serial.println("repeat; ignoring.");
         return;
       }
     } 
-    else if (codeType == SONY) {
-      Serial.print("Received SONY: ");
-    } 
-    else if (codeType == RC5) {
-      Serial.print("Received RC5: ");
-    } 
-    else if (codeType == RC6) {
-      Serial.print("Received RC6: ");
-    } 
-    else {
-      Serial.print("Unexpected codeType ");
-      Serial.print(codeType, DEC);
-      Serial.println("");
+    else
+    {    
+      if (codeType == SONY)
+      {
+        Serial.print("Received SONY: ");
+      } 
+      else
+      {
+        if (codeType == RC5)
+        {
+          Serial.print("Received RC5: ");
+        } 
+        else
+        {
+          if (codeType == RC6)
+          {
+            Serial.print("Received RC6: ");
+          } 
+          else
+          {
+            Serial.print("Unexpected codeType ");
+            Serial.print(codeType, DEC);
+            Serial.println("");
+          }
+        }
+      }
     }
+
     Serial.println(results->value, HEX);
     Serial.println(results->value, DEC);
+    
     codeValue = results->value;
     codeLen = results->bits;
   }
 }
 
-void sendCode(int repeat) {
-  if (codeType == NEC) {
-    if (repeat) {
-      irsend.sendNEC(REPEAT, codeLen);
-      Serial.println("Sent NEC repeat");
-    } 
-    else {
-      irsend.sendNEC(codeValue, codeLen);
-      Serial.print("Sent NEC ");
-      Serial.println(codeValue, HEX);
-    }
-  } 
-  else if (codeType == SONY) {
-    irsend.sendSony(codeValue, codeLen);
-    Serial.print("Sent Sony ");
-    Serial.println(codeValue, HEX);
-  } 
-  else if (codeType == RC5 || codeType == RC6) {
-    if (!repeat) {
-      // Flip the toggle bit for a new button press
-      toggle = 1 - toggle;
-    }
-    // Put the toggle bit into the code to send
-    codeValue = codeValue & ~(1 << (codeLen - 1));
-    codeValue = codeValue | (toggle << (codeLen - 1));
-    if (codeType == RC5) {
-      Serial.print("Sent RC5 ");
-      Serial.println(codeValue, HEX);
-      irsend.sendRC5(codeValue, codeLen);
-    } 
-    else {
-      irsend.sendRC6(codeValue, codeLen);
-      Serial.print("Sent RC6 ");
-      Serial.println(codeValue, HEX);
-    }
-  } 
-  else if (codeType == UNKNOWN /* i.e. raw */) {
-    // Assume 38 KHz
-    irsend.sendRaw(rawCodes, codeLen, 38);
-    Serial.println("Sent raw");
-  }
-}
-
-int lastButtonState;
 
 void loop()
 {
-/*
-  // If button pressed, send the code.
-  int buttonState = digitalRead(BUTTON_PIN);
-  if (lastButtonState == HIGH && buttonState == LOW) {
-    Serial.println("Released");
-    irrecv.enableIRIn(); // Re-enable receiver
-  }
-
-  if (buttonState) {
-    Serial.println("Pressed, sending");
-    digitalWrite(STATUS_PIN, HIGH);
-    sendCode(lastButtonState == buttonState);
-    digitalWrite(STATUS_PIN, LOW);
-    delay(50); // Wait a bit between retransmissions
-  } 
-  else */
+  // wait for IR signal
   if (irrecv.decode(&results))
   {
     // LED on
     digitalWrite(STATUS_PIN, HIGH);
-    
+
+    // store result for convenience    
     storeCode(&results);
     
-    irrecv.resume(); // resume receiver
-    
+    // resume receiver
+    irrecv.resume();
+
+    //---------------------    
     // "translate" to text
+    //---------------------    
     if (codeValue == menu)
     {
         Serial.println("[MENU]");
@@ -247,7 +194,5 @@ void loop()
     // LED off
     digitalWrite(STATUS_PIN, LOW);
   }
-
-//  lastButtonState = buttonState;
 }
 
