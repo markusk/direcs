9c42b0feb453bd2a27073a45bf15e5852e26b249
Markus Knapp <bitbucket@direcs.de>
2015-01-04 20:00:00 +0100

    Code reformatted, #defines from main.h added

--- direcs-arduino/omnibot/omnibot.ino
+++ direcs-arduino/omnibot/omnibot.ino
@@ -1,14 +1,118 @@
-#include "main.h"
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
 
 
+//-------  from main.h  -------------------------------
 
-#ifndef F_CPU
-#define F_CPU 		16000000
-#endif
+//
+// the "serial" commands for the MC
+//
+#define READ_SENSOR_1			1
+#define READ_SENSOR_2			2
+#define READ_SENSOR_3			3
+#define READ_SENSOR_4			4
+#define READ_SENSOR_5			5
+#define READ_SENSOR_6			6
+#define READ_SENSOR_7			7
+#define READ_SENSOR_8			8
+#define READ_SENSOR_16			16
+
+#define	MOTOR1_OFF				20
+#define	MOTOR1_CLOCKWISE		21
+#define	MOTOR1_COUNTERCLOCKWISE	22
+#define	MOTOR2_OFF				23
+#define	MOTOR2_CLOCKWISE		24
+#define	MOTOR2_COUNTERCLOCKWISE	25
+
+#define MOTOR1_SPEED_SET		26
+#define MOTOR2_SPEED_SET		27
+
+#define READ_MOTOR_SENSOR1		28
+#define READ_MOTOR_SENSOR2		29
+
+#define READ_MOTOR_DISTANCE1	30
+#define READ_MOTOR_DISTANCE2	31
+
+#define RESET_MOTOR_DISTANCE1	32
+#define RESET_MOTOR_DISTANCE2	33
+
+#define	FLASHLIGHT_OFF			40
+#define	FLASHLIGHT_ON			41
+
+#define SET_SERVO1				42
+#define SET_SERVO2				43
+#define SET_SERVO3				44
+#define SET_SERVO4				45
+#define SET_SERVO5				46
+#define SET_SERVO6				47
+
+#define MOTOR3_OFF 				50
+#define MOTOR3_CLOCKWISE 		51
+#define MOTOR3_COUNTERCLOCKWISE 52
+#define MOTOR4_OFF 				53
+#define MOTOR4_CLOCKWISE 		54
+#define MOTOR4_COUNTERCLOCKWISE 55
+
+#define MOTOR3_SPEED_SET		56
+#define MOTOR4_SPEED_SET		57
+
+#define SPEED_SET_ALLMOTORS		60
+
+#define	READ_AXIS_X				61
+#define	READ_AXIS_Y				62
+#define	READ_AXIS_Z				63
+
+#define	INIT					64
+#define SLEEP					65
+
+#define READ_CONTACT1			70
+#define READ_CONTACT2			71
+#define READ_CONTACT3			72
+#define READ_CONTACT4			73
+
+/// Some direct driving commmands for the robot. @sa Direcs::drive() @sa Direcs::motorControl()
+#define BOTFORWARD 				80
+#define BOTBACKWARD 			81
+#define BOTLEFT					82
+#define BOTRIGHT				83
+#define BOTTURNLEFT				84
+#define BOTTURNRIGHT			85
+#define BOTSTART				86
+#define BOTSTOP					87
+#define BOTWAIT					88
+// #define BOTDIAGONAL_FORWARD_LEFT
+// #define BOTDIAGONAL_FORWARD_RIGHT
+// #define BOTDIAGONAL_BACKWARD_LEFT
+// #define BOTDIAGONAL_BACKWARD_RIGHT
 
 
-//#include "/usr/local/avr/include/avr/delay.h"
+// these numbers represent the Ports of the A/D converter C  !!!
+// for the infrared sensors and the motor sensors
+#define SENSOR1					0  // ADC0
+#define SENSOR2					1  // ADC1
+#define SENSOR3					2  // ADC2
+#define SENSOR4					3  // ADC3
+#define SENSOR5					4  // ADC4
+#define SENSOR6					5  // ADC5
+#define SENSOR7					6  // ADC6
+#define SENSOR8					7  // ADC7
 
+#define SENSORMOTOR1			38 // ADC14
+#define SENSORMOTOR2			39 // ADC15
+
+// just nice to have
+#define ON						1
+#define OFF						0
+
+// just nice to have
+#define ENABLE					1
+#define DISABLE					0
+
+// "Morsedauer" für ein Bit in Millisekunden
+#define BITZEIT 100     
+
+//--------------------------------------
 
 
 uint8_t leftWheelCounter = 0;
@@ -29,7 +133,14 @@ uint16_t rightDistanceCounter = 0;
 //uint8_t camTiltRSwitch = 0;
 
 
-int main(void)
+void setup()
+{
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+}
+
+
+void loop()
 {
 	// usart stuff
 	RXcompleted = 0;	// Flag, String komplett empfangen
@@ -1190,8 +1301,6 @@ int main(void)
     } // while (1)
 
 
-	// this line is never reached!
-	return 0;
 }
 
 
