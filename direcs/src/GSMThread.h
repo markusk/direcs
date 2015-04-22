95ab6d6a0e5a87af62edff57bd37294ae21e5ec7
Markus Knapp <github@direcs.de>
2015-04-22 19:45:36 +0200

    Deleted old code in GSM thread. Implemented countSMS, setGSMstate, getSMSavailable and setSimulationMode. Disabled heartbeat stuff here. Almost finished.

--- direcs/src/GSMThread.h
+++ direcs/src/GSMThread.h
@@ -26,7 +26,6 @@
 //-------------------------------------------------------------------
 #include <QThread>
 #include <QMutex>
-#include <math.h>
 //-------------------------------------------------------------------
 
 /**
@@ -55,74 +54,9 @@ class GSMThread : public QThread
 		virtual void run();
 
 		/**
-		@param sensor is the sensor number.
-		@return The value of an infrared sensor.
-		*/
-		int getIrSensorValue(int sensor);
-
-		/**
-		@param sensor is the sensor number.
-		@return The value of an ultrasonic sensor.
-		*/
-		int getUsSensorValue(int sensor);
-
-		/**
-		@param sensor is the sensor number.
-		@return The value of a motor sensor.
-		*/
-		int getMotorSensorValue(int sensor);
-
-		/**
-		@param sensor is the sensor number.
-		@return The distance in cm, of a sensor.
-		*/
-		int getDistance(int sensor);
-
-		/**
-		@return The driven distance in cm, of a wheel encoder.
-		@param sensor is the sensor number.
-		*/
-		int getDrivenDistance(int sensor);
-
-		/**
-		Converts a infrared sensor value to a distance in cm.
-		@param sensorValue is the measured sensor value.
-		@return The distance in cm.
-		@sa convertToSensorValue()
-		*/
-		int convertToDistance(int sensorValue);
-
-		/**
-		Converts a distance in cm to an infrared sensor value.
-		@param distance is the distance in cm.
-		@return The sensor value.
-		@sa convertToDistance()
-		*/
-		int convertToSensorValue(int distance);
-
-		/**
-		@return The measured motor sensor value in milli Ampere (mA).
-		@param motor is the motor number.
-		*/
-		int getMAmpere(int motor);
-
-		/**
-		@param sensor is the sensor number and can be VOLTAGESENSOR1 or VOLTAGESENSOR2.
-		@return The (already converted) voltage in Volt (V).
-		*/
-		float getVoltage(int sensor);
-
-		/**
-		@param contact is the contact number.
-		@return The 'measured' contact value. 0 if OFF; unequal to 0 if set.
-		*/
-		int getContactValue(int contact);
-
-		/**
-		@param axis is the x, y or z axis.
-		@return The measured x, y and z value from the 3D magnetic compass module (micromag3).
-		*/
-		int getCompassValue(unsigned char axis);
+		 * @return the no of available SMS's
+		 */
+		int getSMSavailable();
 
 		/**
 		@return The heartbeat value which should look like high=5 or low=0 Volt.
@@ -131,13 +65,6 @@ class GSMThread : public QThread
 
 
 	public slots:
-		/**
-		Resets the driven distance to 0.
-		@param sensor is the sensor number.
-		@return true on success, false on error
-		*/
-		bool resetDrivenDistance(int sensor);
-
 		/**
 		This slot enables or disables the simulation mode.
 		In the simulation mode all sensor values are set to a fixed value. No real sensor values are read from the robot, when set to true.
@@ -154,11 +81,11 @@ class GSMThread : public QThread
 		void setRobotState(bool state);
 
 		/**
-		This slots takes the compass circuit state, to know if the compas module is connected or not.
-		When the class knows this it is unnecessary to communicate with the interface.
+		This slots takes the GSM module state, to know if its down or not.
+		When the class knows this it is unnecessary to communicate with the GSM module.
 		@param state can be true or false
 		*/
-		void setCompassState(bool state);
+		void setGSMState(bool state);
 
 
 	signals:
@@ -166,28 +93,12 @@ class GSMThread : public QThread
 		This signal is emitted when all sensors were read.
 		@sa Direcs::showSensorData()
 		*/
-		void sensorDataComplete();
-
-		/**
-		This signal is emitted when a contact value is read. The result (an alarm) will be displayed immediately in the camera live image.
-		@sa Direcs::showContactAlarm()
-		*/
-		void contactAlarm(char position, bool state);
+		void GSMDataComplete();
 
 		/**
 		Sends a string over the network.
 		*/
 		void sendNetworkString(QString text);
-
-		/**
-		This signal is emitted when all compass values were read.
-		@param x is the x axis value
-		@param y is the y axis value
-		@param z is the z axis value
-		@param heading is the heading of the compass
-		@sa Direcs::showCompassData() GSMThread::calculateHeading()
-		*/
-		void compassDataComplete(float x, float y, float z, float heading);
 
 		/**
 		This signal is emitted every \todo seconds, when a specific value from the microcontroller was received.
@@ -208,78 +119,17 @@ class GSMThread : public QThread
 
 		/**
 		Emits a emergency signal for letting other modules know that we have a massive sensor error. So in that case an emergency stop or so could be initiated.
-		@param errorlevel needs to be defined. Temporariliy we use -2 in case of error in this thread.
+		@param errorlevel needs to be defined. Temporariliy we use -3 in case of error in this thread. -2 is for the sensor thread.
 		*/
 		void systemerror(int errorlevel);
 
 
 	private:
 		/**
-		Converts the compass sensor value (16 bit integer 2's complement format) to degrees (0 to 360).
-		@param sensorValue
-		@return The given value in degrees (0 to 360).
-		*/
-		float convertToDegree(int sensorValue);
-
-		/**
-		Translates the x and y data from the 3D compass into a single orientation reading.
-		*/
-		void calculateHeading(void);
-
-		/**
-		Converts a voltage sensor value to a value in Volt (V).
-		@param sensor is the sensor number.
-		@return The voltage in Volt.
-		*/
-		float convertToVolt(int sensor);
-
-		/**
 		Checks how many SMS are available.
 		@return the amount of available SMS, or -1 in case of error
 		*/
 		int countSMS();
-
-		/**
-		Reads an ultrasonic sensor value from the microcontroller
-		@param sensor is the sensor number (ULTRASONICSENSOR1)
-		@return true on success, false on error
-		*/
-		bool readUltrasonicSensor(short int sensor);
-
-		/**
-		Reads a voltage sensor value from the microcontroller
-		@param sensor is the sensor number (VOLTAGESENSOR1 to VOLTAGESENSOR2)
-		@return true on success, false on error
-		*/
-		bool readVoltageSensor(short int sensor);
-
-		/**
-		Reads a motor sensor value from the microcontroller
-		@param sensor is the sensor number (MOTORSENSOR1 to MOTORSENSOR4)
-		@return true on success, false on error
-		*/
-		bool readMotorSensor(short int sensor);
-
-		/**
-		Reads a driven distance value from the microcontroller
-		@param sensor is the sensor number (MOTORDISTANCE1 to MOTORDISTANCE2)
-		@return true on success, false on error
-		*/
-		bool readDrivenDistance(short int sensor);
-
-		/**
-		Reads an axis value from the 3D compass from the microcontroller
-		@param axis is the axis number (XAXIS, YAXIS or ZAXIS)
-		@return true on success, false on error
-		*/
-		bool readCompassAxis(short int axis);
-
-		/**
-		Reads a contact value from the microcontroller (end contacts, when puhsing something or so)
-		@param contact is the contact number (CONTACT1 to CONTACT4)
-		@return true on success, false on error
-		*/
-		bool readContact(short int contact);
 
 
 		QString className;	/// this will contain the name of _this_ class at runtime for debug messages
@@ -288,28 +138,20 @@ class GSMThread : public QThread
 		volatile bool stopped;
 		bool simulationMode;
 		bool robotState; // stores the robot state within this class
-		bool compassState; // stores the robot state within this class
+		bool GSMState; // stores the GSM module state within this class
 
 		// Every thread sleeps some time, for having a bit more time fo the other threads!
 		// Time in milliseconds
-		static const unsigned long THREADSLEEPTIME = 1000; // Default: 100 ms
+		static const unsigned long THREADSLEEPTIME = 500; // Default: 100 ms?!?
 
-
-		/**
-		for storing the voltage values from the accumulators (power supply)
-		*/
-		int voltageSensorValue[2];
-
-		static const short int VOLTAGESENSOR1 = 0;
-		static const short int VOLTAGESENSOR2 = 1;
+		int availableSMS; /// contains the no of available SMS
 
 		static const int MAXIMUMPLOTHEARTBEAT = 5; /// theoratically 5 Volt for heartbeat "high" @sa Gui::initPlot
 
-
-		/**
+		/*
 		Since a heartbeat can only high or low. we store only 1 value in this array. But we need an array for the plotThread.
 		*/
-		int heartbeatValue[1];
+		//int heartbeatValue[1];
 
 		//
 		// the "serial" commands for the MC
@@ -326,7 +168,8 @@ class GSMThread : public QThread
 		static const unsigned char RED		= 1;
 		static const unsigned char GREEN	= 2;
 
-		qint64 testCounter;
+		static const bool ON  = true;
+		static const bool OFF = false;
 };
 
 #endif
