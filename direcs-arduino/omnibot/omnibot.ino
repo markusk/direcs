0e09e8d5fcf465a029670456f95489d6c3a7806a
Markus Knapp <github@direcs.de>
2015-05-15 22:55:11 +0200

    Updated all motor A, B and Dir pins. Updated pinout file.

--- direcs-arduino/omnibot/omnibot.ino
+++ direcs-arduino/omnibot/omnibot.ino
@@ -16,21 +16,21 @@
 #define relaisPin    13 /// @todo change name (color)
 
 // motor pins
-#define motor1aPin   13
-#define motor1bPin   13
-#define motor1DirPin 13
+#define motor1aPin   22
+#define motor1bPin   23
+#define motor1DirPin 24
 #define motor1PWMPin  2 // all motors same speed
-#define motor2aPin   13
-#define motor2bPin   13
-#define motor2DirPin 13
+#define motor2aPin   25
+#define motor2bPin   26
+#define motor2DirPin 27
 #define motor2PWMPin  2 // all motors same speed
-#define motor3aPin   13
-#define motor3bPin   13
-#define motor3DirPin 13
+#define motor3aPin   28
+#define motor3bPin   29
+#define motor3DirPin 30
 #define motor3PWMPin  2 // all motors same speed
-#define motor4aPin   13
-#define motor4bPin   13
-#define motor4DirPin 13
+#define motor4aPin   31
+#define motor4bPin   32
+#define motor4DirPin 33
 #define motor4PWMPin  2 // all motors same speed
 
 
