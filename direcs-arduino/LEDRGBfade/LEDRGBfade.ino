71d651a3e73fd7bb82014d701cc98f323b1837f7
Markus Knapp <github@direcs.de>
2015-05-16 11:16:57 +0200

    Removing fade. Turning on and of now

--- direcs-arduino/LEDRGBfade/LEDRGBfade.ino
+++ direcs-arduino/LEDRGBfade/LEDRGBfade.ino
@@ -1,35 +1,91 @@
-int ledR1 = 3;           // the pin that the LED is attached to
-int ledG1 = 5;           // the pin that the LED is attached to
-int ledB1 = 6;           // the pin that the LED is attached to
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+// the pins
+int ledR1 =  2;
+int ledG1 =  3;
+int ledB1 =  4;
 
-// the setup routine runs once when you press reset:
+int ledR2 =  5;
+int ledG2 =  6;
+int ledB2 =  7;
+
+int ledR3 =  8;
+int ledG3 =  9;
+int ledB3 = 10;
+
+
+int pause = 750; // ms
+
 void setup()
 {
   // declare pin to be an output:
   pinMode(ledR1, OUTPUT);
+  pinMode(ledR2, OUTPUT);
+  pinMode(ledR3, OUTPUT);
+
   pinMode(ledG1, OUTPUT);
+  pinMode(ledG2, OUTPUT);
+  pinMode(ledG3, OUTPUT);
+
   pinMode(ledB1, OUTPUT);
+  pinMode(ledB2, OUTPUT);
+  pinMode(ledB3, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
+
 void loop()
 {
-  // set the brightness of the pin
-  analogWrite(ledR1, brightness);
-  analogWrite(ledG1, brightness);
-  analogWrite(ledB1, brightness);
+  digitalWrite(ledR1, HIGH);
+  digitalWrite(ledR2, HIGH);
+  digitalWrite(ledR3, HIGH);
 
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
+  digitalWrite(ledG1,  LOW);
+  digitalWrite(ledG2,  LOW);
+  digitalWrite(ledG3,  LOW);
 
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness == 0 || brightness == 255)
-  {
-    fadeAmount = -fadeAmount ;
-  }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+  digitalWrite(ledB1,  LOW);
+  digitalWrite(ledB2,  LOW);
+  digitalWrite(ledB3,  LOW);
+  delay(pause);
+
+
+  digitalWrite(ledR1,  LOW);
+  digitalWrite(ledR2,  LOW);
+  digitalWrite(ledR3,  LOW);
+
+  digitalWrite(ledG1, HIGH);
+  digitalWrite(ledG2, HIGH);
+  digitalWrite(ledG3, HIGH);
+
+  digitalWrite(ledB1,  LOW);
+  digitalWrite(ledB2,  LOW);
+  digitalWrite(ledB3,  LOW);
+  delay(pause);
+
+
+  digitalWrite(ledR1,  LOW);
+  digitalWrite(ledR2,  LOW);
+  digitalWrite(ledR3,  LOW);
+
+  digitalWrite(ledG1,  LOW);
+  digitalWrite(ledG2,  LOW);
+  digitalWrite(ledG3,  LOW);
+
+  digitalWrite(ledB1, HIGH);
+  digitalWrite(ledB2, HIGH);
+  digitalWrite(ledB3, HIGH);
+  delay(pause);
+
+
+  digitalWrite(ledR1, HIGH);
+  digitalWrite(ledR2, HIGH);
+  digitalWrite(ledR3, HIGH);
+
+  digitalWrite(ledG1, HIGH);
+  digitalWrite(ledG2, HIGH);
+  digitalWrite(ledG3, HIGH);
+
+  digitalWrite(ledB1, HIGH);
+  digitalWrite(ledB2, HIGH);
+  digitalWrite(ledB3, HIGH);
+  delay(pause);
 }
 
