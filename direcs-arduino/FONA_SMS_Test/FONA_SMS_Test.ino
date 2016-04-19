8111eb9b69d9ca99827c4120323bf575480b30eb
Markus Knapp <github@direcs.de>
2016-04-19 23:10:13 +0200

    Added comments.

--- direcs-arduino/FONA_SMS_Test/FONA_SMS_Test.ino
+++ direcs-arduino/FONA_SMS_Test/FONA_SMS_Test.ino
@@ -1,22 +1,4 @@
-/***************************************************
-  This is an example for our Adafruit FONA Cellular Module
-
-  Designed specifically to work with the Adafruit FONA
-  ----> http://www.adafruit.com/products/1946
-  ----> http://www.adafruit.com/products/1963
-  ----> http://www.adafruit.com/products/2468
-  ----> http://www.adafruit.com/products/2542
-
-  These cellular modules use TTL Serial to communicate, 2 pins are
-  required to interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
+// For Adafruit FONA
 #include "Adafruit_FONA.h"
 
 // disable debugging!
